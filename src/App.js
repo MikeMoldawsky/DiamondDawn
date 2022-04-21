f08c7c7c19d424d87f222629cdb45b24ae6e7975
Mike Moldawsky <your_donotreply_email_id>
2022-04-21 23:51:03 +0300

    nit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tweezers 2022 - Digital -> Physical -> Digital
         </p>
         <a
           className="App-link"
