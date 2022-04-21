8532309a187f108af001642d74a8ba074e7610c7
Mike Moldawsky <your_donotreply_email_id>
2022-04-22 00:05:07 +0300

    logo

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tweezers 2022 - Digital -> Physical -> Digital
+          Tweezers 2022.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Coming Soon...
       </header>
     </div>
   );
