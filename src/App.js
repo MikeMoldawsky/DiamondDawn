e5bcc09cc9800c6c73320a4b0df9910d575afb13
Mike Moldawsky <your_donotreply_email_id>
2022-04-28 14:48:56 +0300

    send_dm

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './tweezers_logo.png';
-import './App.css';
+import logo from "./tweezers_logo.png";
+import "./App.css";
 
 function App() {
-  const twitterMsgLink = 'https://twitter.com/messages/compose?recipient_id=1441153449328996359&text=I%20would%20like%20to%20join%20the%20Vanguards%20💎';
+  function sendTwitterMsg() {
+    const twitterMsgLink =
+      "https://twitter.com/messages/compose?recipient_id=1441153449328996359&text=I%20would%20like%20to%20join%20the%20Vanguards%20💎";
+    window.open(twitterMsgLink, "_blank");
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <p>
+          <h1>Tweezers 2022.</h1>
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Tweezers 2022.
-        </p>
-        Coming Soon...
-        <p>
-              Are you in the Vanguard List?
-        </p>
-          <a href={twitterMsgLink} target="_blank" rel="noreferrer noopener">
-              Twitter
-          </a>
+        <br />
+        <h2>Coming Soon...</h2>
+        Are you in the Vanguard List?
+        <button onClick={sendTwitterMsg}>Join</button>
       </header>
     </div>
   );
