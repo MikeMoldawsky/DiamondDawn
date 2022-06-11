cb0cfe333a5e07169cae68256b2ed68247b98289
Gal Kleinman <gal.klm@gmail.com>
2022-06-11 19:10:40 +0300

    add journey functionalities and admin journey control functions

--- contracts/PhysicalToDigital.sol
+++ contracts/PhysicalToDigital.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.14;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
@@ -14,22 +14,32 @@ contract PhysicalToDigital is ERC721, Pausable, AccessControl, ERC721Burnable {
     using Counters for Counters.Counter;
 
     struct TokenMetadata {
-        uint8 level;
-        uint8 enhancementsLeft;
+        uint level;
+        uint processesLeft;
     }
 
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    uint8 private constant MAX_LEVEL = 3;
-    uint8 private _currentStage = 0;
     Counters.Counter private _tokenIdCounter;
-    mapping(uint256 => TokenMetadata) private _tokensMetadata;
+    
+    uint private constant MAX_LEVEL = 4;
+    uint public stage;
+    uint public constant MINING_PRICE = 0.1 ether;
+    uint public constant PREPAID_PROCESSING_PRICE = 0.1 ether;
+    uint public processingPrice;
+    bool public isStageActive;
     string[MAX_LEVEL + 1] private _videoUrls;
+    mapping(uint256 => TokenMetadata) private _tokensMetadata;
 
     constructor() ERC721("PhysicalToDigital", "PTD") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
+
+        processingPrice = 0.1 ether;
+        stage = 1;
+        isStageActive = false;
+        _pause();
     }
 
     function _baseURI() internal pure override returns (string memory) {
@@ -69,35 +79,60 @@ contract PhysicalToDigital is ERC721, Pausable, AccessControl, ERC721Burnable {
         return super.supportsInterface(interfaceId);
     }
 
-    function tokenURI(uint256 tokenId) override public view returns (string memory) {
-        // _requireMinted(tokenId);
 
-        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Physical To  Digital", "description": "This is the description of Physical To Digital Project", "image": "https://media.niftygateway.com/video/upload/v1639421141/Andrea/DavidAriew/DecCurated/Mystical_Cabaret_-_David_Ariew_1_wzdhuw.png", "animation_url": "', _getVideoUrl(tokenId), '" }'))));
-        
-        return string(abi.encodePacked('data:application/json;base64,', json));
+    // Custom logics
+
+    function _requireActiveStage() internal view {
+        require(isStageActive, "P2D: A stage should be active to perform this action");
     }
 
-    function _getImageUrl(uint256 tokenId) internal view returns (string memory) {
-        // _requireMinted(tokenId);
-
-        return "https://media.niftygateway.com/video/upload/v1639421141/Andrea/DavidAriew/DecCurated/Mystical_Cabaret_-_David_Ariew_1_wzdhuw.png";
+    function _requireSpecificStage(uint _stage) internal view  {
+        require(stage == _stage, string.concat("P2D: The stage should be ", Strings.toString(_stage), " to perform this action"));
     }
 
-    function _getVideoUrl(uint256 tokenId) internal view returns (string memory) {
-        return _videoUrls[_tokensMetadata[tokenId].level];
+    modifier whenStageIsActive(uint _stage) {
+        _requireActiveStage();
+        _requireSpecificStage(_stage);
+        _;
     }
 
-    function setStage(uint8 stage, string memory videoUrl) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _currentStage = stage;
+    // Admin API
+
+    function setProcessingPrice(uint price) 
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        processingPrice = price;
+    }
+
+    function revealStage(string memory videoUrl)
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        isStageActive = true;
         _videoUrls[stage] = videoUrl;
     }
 
-    function mine(uint8 enhancementsCount) 
+    function completeStage()
         public
-        whenNotPaused
+        onlyRole(DEFAULT_ADMIN_ROLE)
     {
-        // TODO: replace magic number with constant
-        require(enhancementsCount <= MAX_LEVEL, "Enhancements count must be less than or equal to 3");
+        isStageActive = false;
+        stage++;
+    }
+
+    // Client API
+
+    function mine(uint processesPurchased) 
+        public
+        payable
+        whenNotPaused
+        whenStageIsActive(1)
+    {
+        require(processesPurchased <= MAX_LEVEL - 1, string.concat("P2D: Purchased processes should be less than or equal to ", Strings.toString(MAX_LEVEL - 1)));
+        
+        uint price = MINING_PRICE + processesPurchased * PREPAID_PROCESSING_PRICE;
+        require(msg.value == price, string.concat("P2D: Wrong payment - payment should be: ", Strings.toString(price)));
 
         // Regular mint logics
         uint256 tokenId = _tokenIdCounter.current();
@@ -106,23 +141,40 @@ contract PhysicalToDigital is ERC721, Pausable, AccessControl, ERC721Burnable {
         
         // Store token metadata
         _tokensMetadata[tokenId] = TokenMetadata({
-            level: 0,
-            enhancementsLeft: enhancementsCount
+            level: 1,
+            processesLeft: processesPurchased
         });
     }
 
-    function _enhance(uint256 tokenId)
-        // TODO: change to internal after testing 
-        public 
-        whenNotPaused
+    function _process(uint256 tokenId)
+        internal
     {
-        // _requireMinted(tokenId);
         require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner nor approved");
-        require(_tokensMetadata[tokenId].enhancementsLeft > 0, "Insufficient enhancements");
-        // TODO: decide whether it should be _tokensMetadata[tokenId].level = _currentStage - 1
-        require(_tokensMetadata[tokenId].level < _currentStage, "The diamond was already enhanced in this stage");
+        require(_tokensMetadata[tokenId].level == stage - 1, string.concat("P2D: The level of the diamond should be ",  Strings.toString(stage - 1), " to perform this action"));
+
+        if (_tokensMetadata[tokenId].processesLeft == 0) {
+            require(msg.value == processingPrice, string.concat("P2D: Wrong payment - payment should be: ", Strings.toString(processingPrice)));
+        }
 
         _tokensMetadata[tokenId].level++;
-        _tokensMetadata[tokenId].enhancementsLeft--;
+        _tokensMetadata[tokenId].processesLeft--;
+    }
+
+    function polish(uint256 tokenId) 
+        public
+        payable
+        whenNotPaused
+        whenStageIsActive(2)
+    {
+        _process(tokenId);
+    }
+
+    function clean(uint256 tokenId) 
+        public
+        payable
+        whenNotPaused
+        whenStageIsActive(3)
+    {
+        _process(tokenId);
     }
 }
