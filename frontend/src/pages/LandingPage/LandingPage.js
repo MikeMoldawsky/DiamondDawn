eaee692b8fbdb73362a29d9e80274085197627ef
MikeMoldawsky <16150627+MikeMoldawsky@users.noreply.github.com>
2022-06-14 17:03:36 +0300

    Landing page (#35)
    * Install MM
    
    Co-authored-by: Mike Moldawsky <your_donotreply_email_id>
    
    * remove connect
    
    * remove MM
    
    * Hard Hat Project (#13)
    
    * Add node tests
    
    * Ad wallet connect with block native (#15)
    
    * fix env vars (#16)
    
    * add wallet-connect support (#17)
    
    * initial UI (#18)
    
    * initial front
    
    * jsconfig
    
    * use david's video
    
    * [INIT] Introduce PhysicalToDigital Contract (#19)
    
    * [FEAT] Introduce constant tokenURI (#20)
    
    * adjust deploy script to new contract name (#21)
    
    * Initial mine & enhance logics (#22)
    
    * rainbow wallet added (#23)
    
    * set current stage video url on setStage method (#25)
    
    * [FEAT] Diamond Journey Functions & Journey Control Functions (#26)
    
    * landing page (#29)
    
    * Vanguard Approval redirects to twitter (#33)
    
    * Auto stash before merge of "develop" and "origin/develop"
    
    Co-authored-by: Mike Moldawsky <your_donotreply_email_id>
    Co-authored-by: sniirr <sniirr@gmail.com>
    Co-authored-by: Gal Kleinman <33281963+galkleinman@users.noreply.github.com>
    Co-authored-by: samarth gugnani <samarth.gugnani30@gmail.com>

--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import classNames from "classnames";
-import animation from 'assets/video/tweezers-animation.mp4'
+import animation from 'assets/video/animation.mp4'
+// import animation from 'assets/video/tweezers-animation.mp4'
 import tweezersLogo from 'assets/images/logo-with-text.png'
 import ReactPlayer from "react-player";
 import 'css/app.scss'
@@ -8,17 +9,40 @@ import './LandingPage.scss'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
+function sendTwitterMsg() {
+  const twitterMsgLink =
+      "https://twitter.com/messages/compose?recipient_id=1441153449328996359&text=I%20would%20like%20to%20join%20the%20Vanguards%20💎";
+  window.open(twitterMsgLink, "_blank");
+}
+
 const LandingPage = () => {
   const [animate, setAnimate] = useState(false)
+  const [password, setPassword] = useState('')
+  const pwdInput = useRef(null)
 
   useEffect(() => {
     setAnimate(true)
   }, [])
 
+  const onPasswordChange = e => {
+    console.log({e})
+    const pwd = e.target.value
+    setPassword(pwd)
+    if (pwd.length === 6) {
+      pwdInput.current.blur()
+    }
+  }
+
   return (
     <div className={classNames("page landing-page", { animate })}>
+      <header>
+        <div className="logo-box">
+          <div className="by-text">BY</div>
+          <img src={tweezersLogo} alt="TWEEZERS" />
+        </div>
+      </header>
       <div className="centered-content">
-        <h1>Physical <span>2</span> Digital</h1>
+        <h1>A BILLION YEARS IN THE MAKING</h1>
         <ReactPlayer
           url={animation}
           playing
@@ -26,18 +50,22 @@ const LandingPage = () => {
           muted
           loop
           className="react-player"
-          width='100%'
+          width='300px'
           height='auto'
         />
-        <div className="logo-box">
-          <div className="by-text">BY</div>
-          <div>
-            <img src={tweezersLogo} alt="TWEEZERS" />
-          </div>
+        <div className="password-box">
+          <h2>Enter Password</h2>
+          <input ref={pwdInput} type="password" autoFocus className={classNames({filled: password.length > 0})} value={password} onChange={onPasswordChange} maxLength={6} />
         </div>
-        <div className="bottom-text">
-          <div>Request Vanguard Approval <FontAwesomeIcon icon={faPaperPlane} /></div>
-        </div>
+        {/*<div className="logo-box">*/}
+        {/*  <div className="by-text">BY</div>*/}
+        {/*  <div>*/}
+        {/*    <img src={tweezersLogo} alt="TWEEZERS" />*/}
+        {/*  </div>*/}
+        {/*</div>*/}
+        {/*<div className="bottom-text">*/}
+        {/*  <div>Request Vanguard Approval <FontAwesomeIcon icon={faPaperPlane} /></div>*/}
+        {/*</div>*/}
       </div>
     </div>
   )
