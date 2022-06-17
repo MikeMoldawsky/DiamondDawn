d4b6e7ef1fcb3e08ff9f10792a8a338cc55d45ae
Mike Moldawsky <your_donotreply_email_id>
2022-06-17 15:56:16 +0300

    prettier react

--- frontend/src/pages/Wallet.js
+++ frontend/src/pages/Wallet.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import React from 'react'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
 const Wallet = () => {
-  return <ConnectButton accountStatus="address" />;
-};
+  return <ConnectButton accountStatus="address" />
+}
 
-export default Wallet;
+export default Wallet
