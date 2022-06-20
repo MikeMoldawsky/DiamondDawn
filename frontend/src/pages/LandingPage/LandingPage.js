501582d2b4d97b07168b56c9a6ccb3072328a85e
sniirr <sniirr@gmail.com>
2022-06-20 15:58:37 +0300

    new design

--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -5,8 +5,9 @@ import tweezersLogo from 'assets/images/logo-with-text.png'
 import ReactPlayer from "react-player";
 import 'css/app.scss'
 import './LandingPage.scss'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+
+const PASSWORD_LENGTH = 10
+const CHECK_TIME = 1750
 
 const LandingPage = () => {
   const [animate, setAnimate] = useState(false)
@@ -23,15 +24,19 @@ const LandingPage = () => {
     console.log({e})
     const pwd = e.target.value
     setPassword(pwd)
-    if (pwd.length === 6) {
+    if (pwd.length === PASSWORD_LENGTH) {
       pwdInput.current.blur()
       setCheckingPassword(true)
       setTimeout(() => {
         setCheckingPassword(false)
         setPasswordError(true)
         setPassword('')
-      }, 1750)
+      }, CHECK_TIME)
     }
+  }
+
+  const onResultClick = () => {
+    setPasswordError(false)
   }
 
   const onViewClick = () => {
@@ -50,31 +55,36 @@ const LandingPage = () => {
         </div>
       </header>
       <div className="centered-content">
-        <h1>A BILLION YEARS IN THE MAKING</h1>
-        <ReactPlayer
-          url={animation}
-          playing
-          playsinline
-          controls={false}
-          muted
-          loop
-          className="react-player"
-          width='300px'
-          height='auto'
-        />
+        <div className="top-content">
+          <h1>A BILLION YEARS IN THE MAKING</h1>
+          <ReactPlayer
+            url={animation}
+            playing
+            playsinline
+            controls={false}
+            muted
+            loop
+            className="react-player"
+            width='300px'
+            height='auto'
+          />
+          <div className="title">DIAMONDS DAWN</div>
+          <div className="coming-soon">COMING SOON</div>
+        </div>
         {!passwordError ? (
           <div className="password-box">
-            <h2>Enter Password</h2>
+            <div className="pwd-bg"/>
+            <div className="password-title">TRY PASSWORD</div>
             <input ref={pwdInput} type="password" autoFocus
                    className={classNames({filled: password.length > 0, loading: checkingPassword})}
-                   value={password} onChange={onPasswordChange} maxLength={6} />
+                   value={password} onChange={onPasswordChange} maxLength={PASSWORD_LENGTH} />
           </div>
         ) : (
           <div className="password-error">
             <div className="error-message">Wrong Password</div>
             <div className="request-join">
               <a target="_blank" rel="noreferrer" href="https://twitter.com/messages/compose?recipient_id=1441153449328996359&text=I%20would%20like%20to%20join%20the%20Vanguards%20">
-                <div>Request Vanguard Approval <FontAwesomeIcon icon={faPaperPlane} /></div>
+                <div>Request Vanguard Approval</div>
               </a>
             </div>
           </div>
