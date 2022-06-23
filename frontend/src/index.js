ba486e942d717b79715d8be9fcb4b64916599e4d
sniirr <sniirr@gmail.com>
2022-06-24 00:58:29 +0300

    landing page with pwd (#45)
    * Install MM
    
    Co-authored-by: Mike Moldawsky <your_donotreply_email_id>
    
    * remove connect
    
    * remove MM
    
    * Hard Hat Project (#13)
    
    * Add node tests
    
    * Ad wallet connect with block native (#15)
    
    * fix env vars (#16)
    
    * add wallet-connect support (#17)
    
    * initial UI (#18)
    
    * initial front
    
    * jsconfig
    
    * use david's video
    
    * [INIT] Introduce PhysicalToDigital Contract (#19)
    
    * [FEAT] Introduce constant tokenURI (#20)
    
    * adjust deploy script to new contract name (#21)
    
    * Initial mine & enhance logics (#22)
    
    * rainbow wallet added (#23)
    
    * set current stage video url on setStage method (#25)
    
    * [FEAT] Diamond Journey Functions & Journey Control Functions (#26)
    
    * landing page (#29)
    
    * Vanguard Approval redirects to twitter (#33)
    
    * Auto stash before merge of "develop" and "origin/develop"
    
    * [CI] Introduce Github Workflow - Compile & Test & Coverage (#31)
    
    * mobile landing page (#36)
    
    * mobile css
    
    * improve
    
    * try video fix
    
    * Landing page with pwd (#38)
    
    * landing page
    
    * landing page with pwd
    
    * mobile bg
    
    * try fix mobile bg
    
    * again
    
    * and again
    
    * cleanup
    
    * make everything smaller in mobile
    
    * change text
    
    * css fix
    
    * Prettier React & Solidity (#41)
    
    * Update .env.example
    
    * Rename: Diamond Dawn & Cut Stage (#42)
    
    * Stable landing page (#44)
    
    * new design
    
    * touch
    
    * final design
    
    * alt design
    
    Co-authored-by: MikeMoldawsky <16150627+MikeMoldawsky@users.noreply.github.com>
    Co-authored-by: Mike Moldawsky <your_donotreply_email_id>
    Co-authored-by: Gal Kleinman <33281963+galkleinman@users.noreply.github.com>
    Co-authored-by: samarth gugnani <samarth.gugnani30@gmail.com>
    Co-authored-by: MikeMoldawsky <mike.moldawsky@gmail.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import LandingPage from "./pages/LandingPage";
 // import App from "./App";
--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -1,69 +1,110 @@
 import React, { useState, useEffect, useRef } from "react";
 import classNames from "classnames";
-import animation from 'assets/video/animation.mp4'
-import tweezersLogo from 'assets/images/logo-with-text.png'
+import animation from "assets/video/infinity_video.mp4";
+import tweezersLogo from "assets/images/logo.png";
 import ReactPlayer from "react-player";
 import 'css/app.scss'
 import './LandingPage.scss'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
-function sendTwitterMsg() {
-  const twitterMsgLink =
-      "https://twitter.com/messages/compose?recipient_id=1441153449328996359&text=I%20would%20like%20to%20join%20the%20Vanguards%20💎";
-  window.open(twitterMsgLink, "_blank");
-}
+const PASSWORD_LENGTH = 4
+// const CHECK_TIME = 0
+const CHECK_TIME = 1750
 
 const LandingPage = () => {
-  const [animate, setAnimate] = useState(false)
-  const [password, setPassword] = useState('')
-  const pwdInput = useRef(null)
+  const [animate, setAnimate] = useState(false);
+  const [password, setPassword] = useState("");
+  const pwdInput = useRef(null);
+  const [checkingPassword, setCheckingPassword] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
   useEffect(() => {
-    setAnimate(true)
-  }, [])
+    setAnimate(true);
+  }, []);
+
+  const submitPassword = () => {
+    pwdInput.current.blur()
+    // return
+    setCheckingPassword(true)
+    setTimeout(() => {
+      setCheckingPassword(false)
+      setPasswordError(true)
+      setPassword('')
+    }, CHECK_TIME)
+  }
 
   const onPasswordChange = e => {
-    console.log({e})
     const pwd = e.target.value
     setPassword(pwd)
-    if (pwd.length === 6) {
-      pwdInput.current.blur()
+    if (pwd.length === PASSWORD_LENGTH) {
+      submitPassword()
+    }
+  };
+
+  const onPasswordEnter = e => {
+    if (e.charCode === 13) {
+      // enter key
+      submitPassword()
     }
   }
 
+  // const onResultClick = () => {
+  //   setPasswordError(false)
+  // }
+
+  const onViewClick = () => {
+    if (pwdInput && pwdInput.current && !checkingPassword && !passwordError) {
+      pwdInput.current.focus();
+    }
+  };
+
   return (
-    <div className={classNames("page landing-page", { animate })}>
+    <div
+      className={classNames("page landing-page", { animate })}
+      onClick={onViewClick}
+    >
+      <div className="bg" />
       <header>
         <div className="logo-box">
-          <div className="by-text">BY</div>
           <img src={tweezersLogo} alt="TWEEZERS" />
         </div>
       </header>
       <div className="centered-content">
-        <h1>A BILLION YEARS IN THE MAKING</h1>
-        <ReactPlayer
-          url={animation}
-          playing
-          playsinline
-          controls={false}
-          muted
-          loop
-          className="react-player"
-          width='300px'
-          height='auto'
-        />
-        <div className="coming-soon">COMING SOON</div>
-        {/*<div className="password-box">*/}
-        {/*  <h2>Enter Password</h2>*/}
-        {/*  <input ref={pwdInput} type="password" autoFocus className={classNames({filled: password.length > 0})} value={password} onChange={onPasswordChange} maxLength={6} />*/}
-        {/*</div>*/}
-        {/*<div className="bottom-text">*/}
-        {/*  <div>Request Vanguard Approval <FontAwesomeIcon icon={faPaperPlane} /></div>*/}
-        {/*</div>*/}
+        <div className="top-content">
+          <h1>A BILLION YEARS IN THE MAKING</h1>
+          <ReactPlayer
+            url={animation}
+            playing
+            playsinline
+            controls={false}
+            muted
+            // loop
+            className="react-player"
+          />
+          <div className="title">DIAMONDS DAWN</div>
+          <div className="coming-soon">COMING SOON</div>
+        </div>
+        {!passwordError ? (
+          <div className="password-box">
+            <div className="pwd-bg"/>
+            <div className="password-title">TRY PASSWORD</div>
+            <input ref={pwdInput} type="text" autoFocus
+                   className={classNames({filled: password.length > 0, loading: checkingPassword})}
+                   value={password} onChange={onPasswordChange} onKeyPress={onPasswordEnter} maxLength={PASSWORD_LENGTH} />
+          </div>
+        ) : (
+          <div className="password-error">
+            <div className="error-message">Wrong Password</div>
+            <div className="request-join">
+              <a target="_blank" rel="noreferrer" href="https://twitter.com/messages/compose?recipient_id=1441153449328996359&text=I%20would%20like%20to%20join%20the%20Vanguards%20">
+                <div className="request-join-text">Request Vanguards</div>
+              </a>
+            </div>
+          </div>
+        )}
       </div>
+      <div className="remainder" />
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
