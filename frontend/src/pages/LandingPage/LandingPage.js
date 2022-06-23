a112477443213d70e43285bd53ff8002a86b6c48
sniirr <sniirr@gmail.com>
2022-06-23 15:33:25 +0300

    cleanup

--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -13,7 +13,6 @@ const CHECK_TIME = 1750
 const LandingPage = () => {
   const [animate, setAnimate] = useState(false);
   const [password, setPassword] = useState("");
-  const pwdInput = useRef(null);
   const [checkingPassword, setCheckingPassword] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
 
@@ -22,7 +21,6 @@ const LandingPage = () => {
   }, []);
 
   const submitPassword = () => {
-    // pwdInput.current.blur()
     setCheckingPassword(true)
     setTimeout(() => {
       setCheckingPassword(false)
@@ -32,35 +30,18 @@ const LandingPage = () => {
   }
 
   const onPasswordChange = pwd => {
-    // const pwd = e.target.value
     setPassword(pwd)
     if (pwd.length === PASSWORD_LENGTH) {
       submitPassword()
     }
   };
 
-  const onPasswordEnter = e => {
-    if (e.charCode === 13) {
-      // enter key
-      submitPassword()
-    }
-  }
-
-  const onResultClick = () => {
-    setPasswordError(false)
-  }
-
-  const onViewClick = () => {
-    if (pwdInput && pwdInput.current && !checkingPassword && !passwordError) {
-      pwdInput.current.focus();
-    }
-  };
+  // const onResultClick = () => {
+  //   setPasswordError(false)
+  // }
 
   return (
-    <div
-      className={classNames("page landing-page", { animate })}
-      onClick={onViewClick}
-    >
+    <div className={classNames("page landing-page", { animate })}>
       <div className="bg" />
       <header>
         <div className="logo-box">
@@ -76,10 +57,7 @@ const LandingPage = () => {
             playsinline
             controls={false}
             muted
-            // loop
             className="react-player"
-            // width="auto"
-            // height="180px"
           />
           <div className="title">DIAMONDS DAWN</div>
           <div className="coming-soon">COMING SOON</div>
@@ -89,19 +67,10 @@ const LandingPage = () => {
             <div className="pwd-bg"/>
             <div className="password-title">TRY PASSWORD</div>
             <OtpInput containerStyle={classNames("pwd-input")} value={password} onChange={onPasswordChange} numInputs={4} shouldAutoFocus isInputSecure />
-            {/*<input ref={pwdInput} type="password" autoFocus*/}
-            {/*       className={classNames({filled: password.length > 0, loading: checkingPassword})}*/}
-            {/*       value={password} onChange={onPasswordChange} onKeyPress={onPasswordEnter} maxLength={PASSWORD_LENGTH} />*/}
-            {/*<div className="password-underscores">*/}
-            {/*  <div className="underscore" />*/}
-            {/*  <div className="underscore" />*/}
-            {/*  <div className="underscore" />*/}
-            {/*  <div className="underscore" />*/}
-            {/*</div>*/}
           </div>
         ) : (
           <div className="password-error">
-            <div className="error-message" onClick={onResultClick}>Wrong Password</div>
+            <div className="error-message">Wrong Password</div>
             <div className="request-join">
               <a target="_blank" rel="noreferrer" href="https://twitter.com/messages/compose?recipient_id=1441153449328996359&text=I%20would%20like%20to%20join%20the%20Vanguards%20">
                 <div className="request-join-text">Request Vanguards</div>
