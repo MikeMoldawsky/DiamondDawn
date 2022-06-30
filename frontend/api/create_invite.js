f9378f3cc60889364f68b7e85b22402ac37dde99
Mike Moldawsky <mike.moldawsky@gmail.com>
2022-06-30 15:36:00 +0300

    invite-reason

--- frontend/api/create_invite.js
+++ frontend/api/create_invite.js
@@ -1,6 +1,7 @@
 const {createInvite} = require("../db/invite-db-manager");
 
 module.exports = async function (req, res) {
-	const invite = await createInvite();
+	const reason = req.body.reason;
+	const invite = await createInvite(reason);
 	res.send(JSON.stringify(invite));
 };
