b13cba10779f59d6e2955c8297e9183e215b90b9
MikeMoldawsky <mike.moldawsky@gmail.com>
2022-06-30 14:04:42 +0300

    Open Invite API

--- frontend/db/invite-db-manager.js
+++ frontend/db/invite-db-manager.js
@@ -1,11 +1,20 @@
 const InviteModel = require("./models/InviteModel");
 
 
-async function createInviteDocument() {
+async function createInvite() {
 	try {
 		const document = {};
 		const guildCommunityBotModel = new InviteModel(document);
 		return await guildCommunityBotModel.save();
+	} catch (e) {
+		console.log(`Failed to create invite`, e);
+	}
+}
+
+
+async function openInvite(inviteId) {
+	try {
+		return await InviteModel.findOneAndUpdate({_id: inviteId}, {revoked: true, opened: Date.now()});
 	} catch (e) {
 		console.log(`Failed to create invite`, e);
 	}
@@ -22,6 +31,7 @@ async function getInvites() {
 
 
 module.exports = {
-	createInviteDocument,
+	createInvite,
+	openInvite,
 	getInvites
 };
