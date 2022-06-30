353df05be8089e2d92c1d49f4724922c8b3d42d4
Mike Moldawsky <mike.moldawsky@gmail.com>
2022-06-30 14:27:29 +0300

    password

--- frontend/api/open_invite.js
+++ frontend/api/open_invite.js
@@ -3,5 +3,5 @@ const {openInvite} = require("../db/invite-db-manager");
 module.exports = async function (req, res) {
 	const inviteId = req.body.inviteId;
 	const updatedInvite = await openInvite(inviteId);
-	res.send(JSON.stringify({password: "123456"}));
+	res.send(JSON.stringify({password: updatedInvite.password}));
 };
--- frontend/db/models/InviteModel.js
+++ frontend/db/models/InviteModel.js
@@ -1,10 +1,13 @@
+const chance = require('chance')
 const mongoose = require('mongoose')
 const db = require('../db')
 
 const InviteSchema = new mongoose.Schema({
 	revoked: {type: Boolean, required: true, default: false},
 	created: {type: Date, required: true, default: Date.now},
+	password: {type: Number, required: true, default: () => chance.integer({ min: 100000, max: 999999 })},
 	opened: {type: Date},
+
 })
 
 const InviteModel = db.model('Invite', InviteSchema)
