932acca45406f34aec8b3cf382b974ec33707f39
Mike Moldawsky <mike.moldawsky@gmail.com>
2022-06-30 14:00:32 +0300

    fix

--- frontend/api/create_invite.js
+++ frontend/api/create_invite.js
@@ -1,6 +1,6 @@
-const {createInviteDocument} = require("../db/invite-db-manager");
+const {createInvite} = require("../db/invite-db-manager");
 
 module.exports = async function (req, res) {
-	const invite = await createInviteDocument();
+	const invite = await createInvite();
 	res.send(JSON.stringify(invite));
 };
