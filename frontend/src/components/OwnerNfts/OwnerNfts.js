16b5eed901d20216f4ca4357551d9b9be270c95f
samarth30 <samarth.gugnani30@gmail.com>
2022-07-07 02:04:33 +0530

    integrated alchemy sdk

--- frontend/src/components/OwnerNfts/OwnerNfts.js
+++ frontend/src/components/OwnerNfts/OwnerNfts.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./OwnerNfts.scss";
+import { Network, initializeAlchemy, getNftsForOwner } from "@alch/alchemy-sdk";
 import { useDispatch, useSelector } from "react-redux";
 import { systemSelector } from "store/systemReducer";
 import contractAddress from "contracts/contract-address.json";
@@ -12,6 +13,15 @@ const OwnerNfts = () => {
   const { data: addressData } = useAccount();
   const { data: signer } = useSigner();
 
+  // Optional Config object, but defaults to demo api-key and eth-mainnet.
+  const settings = {
+    apiKey: process.env.REACT_ALCHEMY_KEY, // Replace with your Alchemy API Key.
+    network: Network.ETH_MAINNET, // Replace with your network.
+    maxRetries: 10,
+  };
+
+  const alchemy = initializeAlchemy(settings);
+
   const contractConfig = {
     addressOrName: contractAddress.DiamondDawn,
     contractInterface: ddContract.abi,
@@ -23,27 +33,29 @@ const OwnerNfts = () => {
   const [walletNfts, SetwalletNfts] = useState([]);
 
   const loadData = async () => {
-    console.log(provider);
     if (addressData?.address) {
       if (provider?._network?.chainId === 1) {
         let finalNfts = [];
         // for mainnet   "https://eth-mainnet.alchemyapi.io/nft/v2/demo/getNFTs/?owner=${addressData?.address}&contractAddresses[]=${contractAddress.DiamondDawn}"
-        let response = await axios.get(
-          "https://eth-mainnet.alchemyapi.io/nft/v2/demo/getNFTs/?owner=0x9469c98Be5AFD94cD601E094bc401dDD37F480a3&contractAddresses[]=0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"
-        );
-        response = response.data.ownedNfts;
-        response.map((object) => {
-          console.log(object?.id?.tokenId && parseInt(object?.id?.tokenId, 16));
-          finalNfts.push({
-            nftId: object?.id?.tokenId && parseInt(object?.id?.tokenId, 16),
-            tokenUri: object?.tokenUri?.gateway || object?.tokenUri?.raw,
-            metadata: object?.metadata,
+        await getNftsForOwner(
+          alchemy,
+          "0x9469c98Be5AFD94cD601E094bc401dDD37F480a3",
+          {
+            contractAddresses: ["0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"],
+          }
+        ).then((result) => {
+          let response = result?.ownedNfts;
+          response.map((object) => {
+            finalNfts.push({
+              nftId: object?.tokenId,
+              tokenUri: object?.tokenUri?.gateway || object?.tokenUri?.raw,
+              metadata: object?.rawMetadata,
+            });
           });
         });
 
         SetwalletNfts(finalNfts);
       } else {
-        console.log("shello");
         const nftsOwnedByOwner = await contract.walletOfOwner(
           addressData?.address
         );
