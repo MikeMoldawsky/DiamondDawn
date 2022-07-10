c1d43f69f595d8fdc7354ec75c40a9d5682da2c7
sniirr <sniirr@gmail.com>
2022-07-10 13:34:12 +0300

    polish UX

--- frontend/src/pages/App/Polish/Polish.js
+++ frontend/src/pages/App/Polish/Polish.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import _ from 'lodash'
-import Countdown from 'react-countdown';
+import Countdown from 'components/Countdown';
 import { showError } from "utils";
 import useDDContract from "hooks/useDDContract";
 import { BigNumber, utils as ethersUtils } from "ethers";
@@ -8,19 +8,24 @@ import { useSelector } from "react-redux";
 import { uiSelector } from "store/uiReducer";
 import { systemSelector } from "store/systemReducer";
 import { tokenByIdSelector } from "store/tokensReducer";
+import VideoPlayer from "components/VideoPlayer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGem } from "@fortawesome/free-solid-svg-icons";
 
 const Polish = () => {
   const [actionTxId, setActionTxId] = useState(false)
   const contract = useDDContract()
   const { selectedTokenId } = useSelector(uiSelector)
   const token = useSelector(tokenByIdSelector(selectedTokenId))
-  const { polishPrice } = useSelector(systemSelector)
+  const { polishPrice, isStageActive } = useSelector(systemSelector)
+  const [showCompleteVideo, setShowCompleteVideo] = useState(false)
 
   const polish = async () => {
     try {
       const tx = await contract.polish(selectedTokenId, { value: token.polishable ? BigNumber.from(0) : polishPrice })
       const receipt = await tx.wait()
 
+      setShowCompleteVideo(true)
       setActionTxId(receipt.transactionHash)
     }
     catch (e) {
@@ -28,30 +33,48 @@ const Polish = () => {
     }
   }
 
+  const renderContent = () => {
+    const wasPolished = !_.isEmpty(actionTxId)
+
+    if (showCompleteVideo) return (
+      <div onClick={() => setShowCompleteVideo(false)}>
+        <VideoPlayer>05 - POLISH VIDEO</VideoPlayer>
+      </div>
+    )
+
+    if (wasPolished) return (
+      <>
+        <div className="diamond-art">
+          <FontAwesomeIcon icon={faGem} />
+        </div>
+        <div className="leading-text">YOUR PERFECT DIAMOND NFT IS IN YOUR WALLET</div>
+        <Countdown date={Date.now() + 10000} text={['You have', 'until burn']} />
+        <div className="secondary-text">Can it be real?</div>
+      </>
+    )
+
+    return (
+      <>
+        <div className="diamond-art">
+          <FontAwesomeIcon icon={faGem} />
+        </div>
+        <div className="leading-text">
+          A GEM CANNOT BE POLISHED WITHOUT FRICTION,<br/>
+          NOR MAN PERFECTED WITHOUT TRIALS
+        </div>
+        <div className="secondary-text">Discover the beauty, a billion years in the making</div>
+        {isStageActive ? (
+          <div className="button action-button" onClick={polish}>POLISH{token.polishable ? '' : ` (${ethersUtils.formatUnits(polishPrice)} ETH)`}</div>
+        ) : (
+          <Countdown date={Date.now() + 10000} text={['You have', 'until polish']} />
+        )}
+      </>
+    )
+  }
+
   return (
     <div className="action-view mine">
-      <div className="leading-text">Polish is open:</div>
-      <div className="action">
-        {_.isEmpty(actionTxId)
-          ? (
-            <>
-              <div className="button action-button" onClick={polish}>POLISH{token.polishable ? '' : ` (${ethersUtils.formatUnits(polishPrice)} ETH)`}</div>
-              <div className="leading-text">Polish ends in:</div>
-            </>
-          )
-          : (<div className="result">
-              <div>Polish Success!</div>
-              <div>TX: {actionTxId}</div>
-              <div className="next-stage">
-                <div className="leading-text">Burn stage will be open in:</div>
-              </div>
-            </div>
-          )
-        }
-        <div className="countdown">
-          <Countdown date={Date.now() + 10000} />
-        </div>
-      </div>
+      {renderContent()}
     </div>
   )
 }
