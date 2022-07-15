29b349136b358c7d6b276fd56b6314c320b69cf0
Gal Kleinman <gal.klm@gmail.com>
2022-07-15 12:23:10 +0300

    code restructure

--- contracts/DiamondDawn.sol
+++ contracts/DiamondDawn.sol
@@ -12,7 +12,10 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
 
-/// @custom:security-contact tweezers@gmail.com
+/**
+ * @title DiamondDawn NFT Contract 
+ * @author Diamond Dawn
+ */
 contract DiamondDawn is
     ERC721,
     ERC2981,
@@ -48,10 +51,7 @@ contract DiamondDawn is
 
     event StageChanged(Stage stage, bool isStageActive);
 
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
-
     Stage private constant MAX_STAGE = Stage.REBIRTH;
     Stage public stage;
     uint public constant MINING_PRICE = 0.2 ether;
@@ -61,48 +61,25 @@ contract DiamondDawn is
     uint public constant PREPAID_POLISH_PRICE = 0.4 ether;
     bool public isStageActive;
     mapping(address => bool) public mintAllowedAddresses;
-
     mapping(Stage => string) private _videoUrls;
     mapping(uint256 => Metadata) private _tokensMetadata;
     mapping(uint256 => address) private _burnedTokenToOwner;
     mapping(address => EnumerableSet.UintSet) private _ownerToBurnedTokens;
 
+    /**************************************************************************
+     *                                                                        *
+     *                               General                                  *
+     *                                                                        *
+     **************************************************************************/
+
     constructor(uint96 _royaltyFeesInBips) ERC721("DiamondDawn", "DD") {
         _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        _grantRole(PAUSER_ROLE, _msgSender());
-        _grantRole(MINTER_ROLE, _msgSender());
 
         mintAllowedAddresses[_msgSender()] = true;
         stage = Stage.MINE;
         isStageActive = false;
         setRoyaltyInfo(_msgSender(), _royaltyFeesInBips);
         _pause();
-    }
-
-    function setRoyaltyInfo(address _receiver, uint96 _royaltyFeesInBips)
-        public
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
-        _setDefaultRoyalty(_receiver, _royaltyFeesInBips);
-    }
-
-    function _baseURI() internal pure override returns (string memory) {
-        return
-            "https://tweezers-public.s3.amazonaws.com/diamond-dawn-nft-mocks/";
-    }
-
-    function pause() public onlyRole(PAUSER_ROLE) {
-        _pause();
-    }
-
-    function unpause() public onlyRole(PAUSER_ROLE) {
-        _unpause();
-    }
-
-    function safeMint(address to) public onlyRole(MINTER_ROLE) {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
     }
 
     function _beforeTokenTransfer(
@@ -114,115 +91,12 @@ contract DiamondDawn is
     }
 
     // The following functions are overrides required by Solidity.
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
+    function supportsInterface(bytes4 interfaceId) public view
         override(ERC721, ERC721Enumerable, AccessControl, ERC2981)
         returns (bool)
     {
         // EIP2981 supported for royalties
         return super.supportsInterface(interfaceId);
-    }
-
-    function walletOfOwner(address _owner)
-        public
-        view
-        returns (uint256[] memory)
-    {
-        uint256 ownerTokenCount = balanceOf(_owner);
-        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
-        for (uint256 i; i < ownerTokenCount; i++) {
-            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
-        }
-        return tokenIds;
-    }
-
-    function getBurnedTokens(address owner)
-        public
-        view
-        returns (uint256[] memory)
-    {
-        return _ownerToBurnedTokens[owner].values();
-    }
-
-    // Custom logics
-    function _requireActiveStage() internal view {
-        require(
-            isStageActive,
-            "A stage should be active to perform this action"
-        );
-    }
-
-    function _requireSpecificStage(Stage _stage) internal view {
-        require(
-            stage == _stage,
-            string.concat(
-                "The stage should be ",
-                Strings.toString(uint(_stage)),
-                " to perform this action"
-            )
-        );
-    }
-
-    modifier _requireAllowedMiner() {
-        require(
-            mintAllowedAddresses[_msgSender()],
-            "The miner is not allowed to mint tokens"
-        );
-        _;
-    }
-
-    function _requireValidProcessesPurchased(uint processesPurchased)
-        internal
-        pure
-    {
-        require(
-            processesPurchased <= uint(MAX_STAGE) - 1,
-            string.concat(
-                "Purchased processes should be less than or equal to ",
-                Strings.toString(uint(MAX_STAGE) - 1)
-            )
-        );
-    }
-
-    function _requireValidPayment(uint processesPurchased, uint value)
-        internal
-        pure
-    {
-        uint price = MINING_PRICE;
-        if (processesPurchased > 0) {
-            price += PREPAID_CUT_PRICE;
-        }
-        if (processesPurchased > 1) {
-            price += PREPAID_POLISH_PRICE;
-        }
-
-        require(
-            value == price,
-            string.concat(
-                "Wrong payment - payment should be: ",
-                Strings.toString(price)
-            )
-        );
-    }
-
-    modifier whenStageIsActive(Stage _stage) {
-        _requireActiveStage();
-        _requireSpecificStage(_stage);
-        _;
-    }
-
-    modifier whenRebirthIsActive() {
-        require(
-            (stage == Stage.PHYSICAL && isStageActive) ||
-                stage == Stage.REBIRTH,
-            "A stage should be active to perform this action"
-        );
-        _;
-    }
-
-    function _getNextStageForToken(uint tokenId) internal view returns (Stage) {
-        return _getNextStage(_tokensMetadata[tokenId].stage);
     }
 
     function _getNextStage(Stage _stage) internal pure returns (Stage) {
@@ -237,41 +111,62 @@ contract DiamondDawn is
         return Stage(uint(_stage) + 1);
     }
 
-    // Admin API - Write
+    /**************************************************************************
+     *                                                                        *
+     *                       Administrator functions                          *
+     *                                                                        *
+     **************************************************************************/
 
-    // Internal
+    /**********************     Internal & Helpers     ************************/
 
-    function _activateStage() internal onlyRole(DEFAULT_ADMIN_ROLE) {
+    function _activateStage() internal
+        onlyRole(DEFAULT_ADMIN_ROLE) 
+    {
         isStageActive = true;
     }
 
-    function _nextStage() internal onlyRole(DEFAULT_ADMIN_ROLE) {
+    function _nextStage() internal 
+        onlyRole(DEFAULT_ADMIN_ROLE) 
+    {
         stage = _getNextStage(stage);
     }
 
-    function _deactivateStage() internal onlyRole(DEFAULT_ADMIN_ROLE) {
+    function _deactivateStage() internal 
+        onlyRole(DEFAULT_ADMIN_ROLE) 
+    {
         isStageActive = false;
     }
 
-    function _assignCurrentStageVideo(string memory videoUrl)
-        internal
+    function _assignCurrentStageVideo(string memory videoUrl) internal
         onlyRole(DEFAULT_ADMIN_ROLE)
     {
         _videoUrls[stage] = videoUrl;
     }
 
-    // Public
+    /**********************        Transactions        ************************/
 
-    function setProcessingPrice(uint price)
-        public
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
-        //        processingPrice = price;
+    function safeMint(address to) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
     }
 
-    function revealStage(string memory videoUrl)
-        public
-    //        onlyRole(DEFAULT_ADMIN_ROLE)
+    function setRoyaltyInfo(address _receiver, uint96 _royaltyFeesInBips) public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        _setDefaultRoyalty(_receiver, _royaltyFeesInBips);
+    }
+
+    function pause() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _unpause();
+    }
+
+    function revealStage(string memory videoUrl) public
+        onlyRole(DEFAULT_ADMIN_ROLE)
     {
         _activateStage();
         _assignCurrentStageVideo(videoUrl);
@@ -279,7 +174,8 @@ contract DiamondDawn is
         emit StageChanged(stage, isStageActive);
     }
 
-    function completeCurrentStage() public //    onlyRole(DEFAULT_ADMIN_ROLE)
+    function completeCurrentStage() public 
+        onlyRole(DEFAULT_ADMIN_ROLE)
     {
         _deactivateStage();
         _nextStage();
@@ -287,15 +183,14 @@ contract DiamondDawn is
         emit StageChanged(stage, isStageActive);
     }
 
-    function completeCurrentStageAndRevealNextStage(
-        string memory videoUrl //    onlyRole(DEFAULT_ADMIN_ROLE)
-    ) public {
+    function completeCurrentStageAndRevealNextStage(string memory videoUrl ) public 
+        onlyRole(DEFAULT_ADMIN_ROLE) 
+    {
         completeCurrentStage();
         revealStage(videoUrl);
     }
 
-    function addToAllowList(address[] memory addresses)
-        public
+    function addToAllowList(address[] memory addresses) public
         onlyRole(DEFAULT_ADMIN_ROLE)
     {
         for (uint i = 0; i < addresses.length; i++) {
@@ -303,6 +198,7 @@ contract DiamondDawn is
         }
     }
 
+    // TODO: Remove this method before production
     function dev__ResetStage() public {
         stage = Stage(0);
         isStageActive = false;
@@ -310,32 +206,22 @@ contract DiamondDawn is
         emit StageChanged(stage, isStageActive);
     }
 
-    // Client API - Write
+    /**********************            Read            ************************/
 
-    function mine(uint processesPurchased)
-        public
-        payable
-        whenStageIsActive(Stage.MINE)
-        _requireAllowedMiner
-    {
-        _requireValidProcessesPurchased(processesPurchased);
-        _requireValidPayment(processesPurchased, msg.value);
+    /**************************************************************************
+     *                                                                        *
+     *                             Client functions                           *
+     *                                                                        *
+     **************************************************************************/
 
-        // Regular mint logics
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(_msgSender(), tokenId);
+    /**********************     Internal & Helpers     ************************/
 
-        // Store token metadata
-        _tokensMetadata[tokenId] = Metadata({
-            stage: Stage.MINE,
-            cutable: processesPurchased >= 1,
-            polishable: processesPurchased == 2,
-            shape: Shape.ROUGH
-        });
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://tweezers-public.s3.amazonaws.com/diamond-dawn-nft-mocks/";
+    }
 
-        // Restrict another mint by the same miner
-        delete mintAllowedAddresses[_msgSender()];
+    function _getNextStageForToken(uint tokenId) internal view returns (Stage) {
+        return _getNextStage(_tokensMetadata[tokenId].stage);
     }
 
     function _process(uint256 tokenId, uint processingPrice) internal {
@@ -368,24 +254,157 @@ contract DiamondDawn is
         _tokensMetadata[tokenId].stage = _getNextStageForToken(tokenId);
     }
 
-    function cut(uint256 tokenId) public payable whenStageIsActive(Stage.CUT) {
+    function _getVideoUrl(uint256 tokenId) internal view returns (string memory)
+    {
+        return
+            string.concat(
+                _baseURI(),
+                _videoUrls[_tokensMetadata[tokenId].stage]
+            );
+    }
+
+    function _getRandomNumber() internal view returns (uint) {
+        uint randomNumber = _randomModulo(100);
+        // 0  - 34 it will be shape 1
+        // 35 - 70 it will be shape 2
+        // 70 - 99 it will be shape 3
+        if (randomNumber <= 34) {
+            return 0;
+        } else if (randomNumber >= 35 && randomNumber <= 70) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    function _randomModulo(uint modulo) internal view returns (uint) {
+        return
+            uint(
+                keccak256(abi.encodePacked(block.timestamp, block.difficulty))
+            ) % modulo;
+    }
+
+    /**********************           Guards            ************************/
+
+    function _requireActiveStage() internal view {
+        require(
+            isStageActive,
+            "A stage should be active to perform this action"
+        );
+    }
+
+    function _requireSpecificStage(Stage _stage) internal view {
+        require(
+            stage == _stage,
+            string.concat(
+                "The stage should be ",
+                Strings.toString(uint(_stage)),
+                " to perform this action"
+            )
+        );
+    }
+
+    function _requireValidPayment(uint processesPurchased, uint value)
+        internal
+        pure
+    {
+        uint price = MINING_PRICE;
+        if (processesPurchased > 0) {
+            price += PREPAID_CUT_PRICE;
+        }
+        if (processesPurchased > 1) {
+            price += PREPAID_POLISH_PRICE;
+        }
+
+        require(
+            value == price,
+            string.concat(
+                "Wrong payment - payment should be: ",
+                Strings.toString(price)
+            )
+        );
+    }
+
+    function _requireValidProcessesPurchased(uint processesPurchased)
+        internal
+        pure
+    {
+        require(
+            processesPurchased <= uint(MAX_STAGE) - 1,
+            string.concat(
+                "Purchased processes should be less than or equal to ",
+                Strings.toString(uint(MAX_STAGE) - 1)
+            )
+        );
+    }
+
+    /**********************          Modifiers          ************************/
+
+    modifier _requireAllowedMiner() {
+        require(
+            mintAllowedAddresses[_msgSender()],
+            "The miner is not allowed to mint tokens"
+        );
+        _;
+    }
+
+    modifier whenStageIsActive(Stage _stage) {
+        _requireActiveStage();
+        _requireSpecificStage(_stage);
+        _;
+    }
+
+    modifier whenRebirthIsActive() {
+        require(
+            (stage == Stage.PHYSICAL && isStageActive) ||
+                stage == Stage.REBIRTH,
+            "A stage should be active to perform this action"
+        );
+        _;
+    }
+
+    /**********************        Transactions        ************************/
+
+    function mine(uint processesPurchased) public payable
+        whenStageIsActive(Stage.MINE)
+        _requireAllowedMiner
+    {
+        _requireValidProcessesPurchased(processesPurchased);
+        _requireValidPayment(processesPurchased, msg.value);
+
+        // Regular mint logics
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(_msgSender(), tokenId);
+
+        // Store token metadata
+        _tokensMetadata[tokenId] = Metadata({
+            stage: Stage.MINE,
+            cutable: processesPurchased >= 1,
+            polishable: processesPurchased == 2,
+            shape: Shape.ROUGH
+        });
+
+        // Restrict another mint by the same miner
+        delete mintAllowedAddresses[_msgSender()];
+    }
+
+    function cut(uint256 tokenId) public payable 
+        whenStageIsActive(Stage.CUT) 
+    {
         _process(tokenId, CUT_PRICE);
 
         uint randomNumber = _getRandomNumber();
         _tokensMetadata[tokenId].shape = Shape(randomNumber);
     }
 
-    function polish(uint256 tokenId)
-        public
-        payable
+    function polish(uint256 tokenId) public payable
         whenStageIsActive(Stage.POLISH)
     {
         _process(tokenId, POLISH_PRICE);
     }
 
-    function burn(uint256 tokenId)
-        public
-        override
+    function burn(uint256 tokenId) public override
         whenStageIsActive(Stage.PHYSICAL)
     {
         super.burn(tokenId);
@@ -408,14 +427,33 @@ contract DiamondDawn is
         _safeMint(_msgSender(), tokenId);
     }
 
-    // Client API - Read
+    /**********************            Read            ************************/
+
+    function walletOfOwner(address _owner) public view returns (uint256[] memory)
+    {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+        for (uint256 i; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokenIds;
+    }
+
+    function getBurnedTokens(address owner) public view returns (uint256[] memory)
+    {
+        return _ownerToBurnedTokens[owner].values();
+    }
+
+    function getShapeForToken(uint tokenId) public view returns (Shape) {
+        return _tokensMetadata[tokenId].shape;
+    }
+
     function tokenURI(uint256 tokenId)
         public
         view
         override
         returns (string memory)
     {
-        // _requireMinted(tokenId);
         string memory videoUrl = _getVideoUrl(tokenId);
         string memory json = Base64.encode(
             bytes(
@@ -438,43 +476,7 @@ contract DiamondDawn is
                 )
             )
         );
+        
         return string(abi.encodePacked("data:application/json;base64,", json));
-    }
-
-    function _getVideoUrl(uint256 tokenId)
-        internal
-        view
-        returns (string memory)
-    {
-        return
-            string.concat(
-                _baseURI(),
-                _videoUrls[_tokensMetadata[tokenId].stage]
-            );
-    }
-
-    function getShapeForToken(uint tokenId) public view returns (Shape) {
-        return _tokensMetadata[tokenId].shape;
-    }
-
-    function _getRandomNumber() internal view returns (uint) {
-        uint randomNumber = _randomModulo(100);
-        // 0  - 34 it will be shape 1
-        // 35 - 70 it will be shape 2
-        // 70 - 99 it will be shape 3
-        if (randomNumber <= 34) {
-            return 0;
-        } else if (randomNumber >= 35 && randomNumber <= 70) {
-            return 1;
-        } else {
-            return 2;
-        }
-    }
-
-    function _randomModulo(uint modulo) internal view returns (uint) {
-        return
-            uint(
-                keccak256(abi.encodePacked(block.timestamp, block.difficulty))
-            ) % modulo;
     }
 }
