9ba84f8e60a83f499eb3194d20fdaa146d4cec8e
Mike Moldawsky <mike.moldawsky@gmail.com>
2022-07-19 14:14:38 +0300

    db

--- db/db.js
+++ db/db.js
@@ -1,6 +1,30 @@
 const mongoose = require("mongoose");
 
-const conn = mongoose.createConnection(process.env.MONGODB_URI);
+function getDbConnection() {
+  console.log("MIKE PARAMSSSS", {
+    vercelenv: process.env.VERCEL_ENV,
+    ref: process.env.VERCEL_GIT_COMMIT_REF,
+    author: process.env.VERCEL_GIT_COMMIT_AUTHOR_LOGIN,
+  });
+  if (
+    process.env.VERCEL_ENV === "preview" &&
+    process.env.VERCEL_GIT_COMMIT_REF !== "develop"
+  ) {
+    switch (process.env.VERCEL_GIT_COMMIT_AUTHOR_LOGIN) {
+      case "mike":
+        return mongoose.createConnection(process.env.MONGODB_URI);
+      case "asaf":
+        return mongoose.createConnection(process.env.MONGODB_URI);
+      default:
+        throw Error(
+          "MongoDB url is required for preview mode that is not on develop"
+        );
+    }
+  }
+  return mongoose.createConnection(process.env.MONGODB_URI);
+}
+
+const conn = getDbConnection();
 
 // Bind connection to error event (to get notification of connection errors)
 conn.on("error", console.error.bind(console, "MongoDB connection error:"));
