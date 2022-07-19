d842baf572e20774083a705b02cc0c365c9437b1
samarth30 <samarth.gugnani30@gmail.com>
2022-07-19 22:07:06 +0530

    moved token uri function to new smart contract

--- contracts/Diamond.sol
+++ contracts/Diamond.sol
@@ -2,15 +2,50 @@
 pragma solidity ^0.8.14;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 /**
  * @title DiamondDawn NFT Contract
  * @author Diamond Dawn
  */
-contract Diamond {
+contract Diamond is AccessControl {
     address public DiamondDawnNft;
 
-    constructor(address _diamondDawnNft) {
-        DiamondDawnNft = _diamondDawnNft;
+    constructor() {
+        grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function tokenURI(
+        uint256 tokenId,
+        string calldata _videoUrl,
+        uint256 _stage,
+        uint256 _shape,
+        string calldata _cutable,
+        string calldata _polishable
+    ) public view returns (string memory) {
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "Diamond Dawn", "description": "This is the description of Diamond Dawn Project", "image": "',
+                        _videoUrl,
+                        '", "animation_url": "',
+                        _videoUrl,
+                        '", "stage": ',
+                        Strings.toString(_stage),
+                        ', "shape": ',
+                        Strings.toString(_shape),
+                        ', "cutable": ',
+                        _cutable,
+                        ', "polishable": ',
+                        _polishable,
+                        " }"
+                    )
+                )
+            )
+        );
+
+        return string(abi.encodePacked("data:application/json;base64,", json));
     }
 }
