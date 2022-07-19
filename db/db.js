7b8a9745fc1f2119a8a9b4a22e8b48a8ddbee288
sniirr <sniirr@gmail.com>
2022-07-19 15:51:30 +0300

    fix vercel preview

--- db/db.js
+++ db/db.js
@@ -9,7 +9,7 @@ function getDbConnection() {
       case "MikeMoldawsky":
         return mongoose.createConnection(process.env.MONGODB_URI_MIKE);
       case "sniirr":
-        return mongoose.createConnection(process.env.MONGODB_URI_ASSAF);
+        return mongoose.createConnection(process.env.MONGODB_URI_ASAF);
       case "galkleinman":
         throw Error("Gal send to Mike a mongo url for development");
       case "smarth30":
