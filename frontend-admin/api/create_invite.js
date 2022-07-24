8cf3d0dc7577cd94380d98a92400d8e8d2eb86f9
Mike Moldawsky <mike.moldawsky@gmail.com>
2022-07-24 16:16:03 +0300

    package lock

--- frontend-admin/api/create_invite.js
+++
@@ -1,6 +0,0 @@
-const { createInvite } = require("../db/invite-db-manager");
-
-module.exports = async function (req, res) {
-  const invite = await createInvite();
-  res.send(JSON.stringify(invite));
-};
