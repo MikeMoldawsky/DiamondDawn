de1c78856a265bc452b599f9764c246971b10193
Gal Kleinman <33281963+galkleinman@users.noreply.github.com>
2022-07-25 12:43:39 +0300

    [FIX] Contracts DB persistency issue (#106)

--- db/contract-db-manager.js
+++ db/contract-db-manager.js
@@ -4,19 +4,19 @@ const DIAMOND_DAWN_CONTRACT_NAME = "DiamondDawn";
 const DIAMOND_DAWN_MINE_CONTRACT_NAME = "DiamondDawnMine";
 
 async function updateDiamondDawnContract(address, artifact) {
-  updateContractByName(DIAMOND_DAWN_CONTRACT_NAME, address, artifact);
+  await updateContractByName(DIAMOND_DAWN_CONTRACT_NAME, address, artifact);
 }
 
 async function updateDiamondDawnMineContract(address, artifact) {
-  updateContractByName(DIAMOND_DAWN_MINE_CONTRACT_NAME, address, artifact);
+  await updateContractByName(DIAMOND_DAWN_MINE_CONTRACT_NAME, address, artifact);
 }
 
 async function getDiamondDawnContract() {
-  getContractByName(DIAMOND_DAWN_CONTRACT_NAME);
+  return await getContractByName(DIAMOND_DAWN_CONTRACT_NAME);
 }
 
 async function getDiamondDawnMineContract() {
-  getContractByName(DIAMOND_DAWN_MINE_CONTRACT_NAME);
+  return await getContractByName(DIAMOND_DAWN_MINE_CONTRACT_NAME);
 }
 
 async function getContractByName(name) {
