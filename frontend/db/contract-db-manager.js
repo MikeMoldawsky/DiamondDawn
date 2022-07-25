fdb2c0cac8fd023bb7f67e54262a93bf2b62f5e9
MikeMoldawsky <mike.moldawsky@gmail.com>
2022-07-25 17:36:36 +0300

    Fix db contracts (#110)

--- frontend/db/contract-db-manager.js
+++ frontend/db/contract-db-manager.js
@@ -1,22 +1,9 @@
 const ContractModel = require("./models/ContractModel");
 
 const DIAMOND_DAWN_CONTRACT_NAME = "DiamondDawn";
-const DIAMOND_DAWN_MINE_CONTRACT_NAME = "DiamondDawnMine";
-
-async function updateDiamondDawnContract(address, artifact) {
-  await updateContractByName(DIAMOND_DAWN_CONTRACT_NAME, address, artifact);
-}
-
-async function updateDiamondDawnMineContract(address, artifact) {
-  await updateContractByName(DIAMOND_DAWN_MINE_CONTRACT_NAME, address, artifact);
-}
 
 async function getDiamondDawnContract() {
   return await getContractByName(DIAMOND_DAWN_CONTRACT_NAME);
-}
-
-async function getDiamondDawnMineContract() {
-  return await getContractByName(DIAMOND_DAWN_MINE_CONTRACT_NAME);
 }
 
 async function getContractByName(name) {
@@ -27,18 +14,6 @@ async function getContractByName(name) {
   }
 }
 
-async function updateContractByName(name, address, artifact) {
-  try {
-    const document = { name, address, artifact };
-    await ContractModel.findOneAndUpdate({ name }, document, { upsert: true });
-  } catch (e) {
-    throw new Error(`Failed to update contract ${name}: ${e}`);
-  }
-}
-
 module.exports = {
-  updateDiamondDawnContract,
-  updateDiamondDawnMineContract,
-  getDiamondDawnContract,
-  getDiamondDawnMineContract
+  getDiamondDawnContract
 };
