e852c98c006fd5c49fe6c0fbeadd1e634723578a
MikeMoldawsky <mike.moldawsky@gmail.com>
2022-07-25 16:37:05 +0300

    Add env vars (#109)

--- smart-contracts/db/db.js
+++ smart-contracts/db/db.js
@@ -2,26 +2,26 @@ const mongoose = require("mongoose");
 
 function getDbConnection() {
   console.log("Connecting to MongoDB...");
-  
-  if (
-    process.env.VERCEL_ENV === "preview" &&
-    process.env.VERCEL_GIT_COMMIT_REF !== "develop"
-  ) {
-    switch (process.env.VERCEL_GIT_COMMIT_AUTHOR_LOGIN) {
-      case "MikeMoldawsky":
-        return mongoose.createConnection(process.env.MONGODB_URI_MIKE);
-      case "sniirr":
-        return mongoose.createConnection(process.env.MONGODB_URI_ASAF);
-      case "galkleinman":
-        throw mongoose.createConnection(process.env.MONGODB_URI_GAL);
-      case "smarth30":
-        throw Error("Samarth send to Mike a mongo url for development");
-      default:
-        throw Error(
-          "MongoDB url is required for preview mode that is not on develop"
-        );
-    }
-  }
+  //
+  // if (
+  //   process.env.VERCEL_ENV === "preview" &&
+  //   process.env.VERCEL_GIT_COMMIT_REF !== "develop"
+  // ) {
+  //   switch (process.env.VERCEL_GIT_COMMIT_AUTHOR_LOGIN) {
+  //     case "MikeMoldawsky":
+  //       return mongoose.createConnection(process.env.MONGODB_URI_MIKE);
+  //     case "sniirr":
+  //       return mongoose.createConnection(process.env.MONGODB_URI_ASAF);
+  //     case "galkleinman":
+  //       throw mongoose.createConnection(process.env.MONGODB_URI_GAL);
+  //     case "smarth30":
+  //       throw Error("Samarth send to Mike a mongo url for development");
+  //     default:
+  //       throw Error(
+  //         "MongoDB url is required for preview mode that is not on develop"
+  //       );
+  //   }
+  // }
   return mongoose.createConnection(process.env.MONGODB_URI);
 }
 
