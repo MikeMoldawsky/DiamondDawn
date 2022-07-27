5fb74d38d76bd2db21982a48d79a4729625c6023
sniirr <sniirr@gmail.com>
2022-07-27 10:34:56 +0300

    add mine contract to admin

--- frontend-admin/src/hooks/useDDContract.js
+++ frontend-admin/src/hooks/useDDContract.js
@@ -1,18 +1,19 @@
 import { useContract, useProvider, useSigner } from "wagmi";
 import { useSelector } from "react-redux";
-import { systemSelector } from "store/systemReducer";
+import { contractSelector } from "store/systemReducer";
+import { CONTRACTS } from 'consts'
 
-function useDDContract() {
-  const { ddContractData } = useSelector(systemSelector)
+function useDDContract(contractType = CONTRACTS.DiamondDawn) {
+  const contractData = useSelector(contractSelector(contractType))
   const provider = useProvider()
   const { data: signer } = useSigner()
 
   const contractConfig = {
-    addressOrName: ddContractData.address,
-    contractInterface: ddContractData.artifact.abi,
+    addressOrName: contractData.address,
+    contractInterface: contractData.artifact.abi,
     signerOrProvider: signer || provider,
   }
-  console.log("Using DiamondDawn client config ", {address: contractConfig.addressOrName, signerOrProvider: contractConfig.signerOrProvider});
+  console.log(`Using ${contractType} client config `, {address: contractConfig.addressOrName, signerOrProvider: contractConfig.signerOrProvider});
   return useContract(contractConfig)
 }
 
