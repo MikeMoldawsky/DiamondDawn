581c5a58341a1ff987f6854787d894714610c5a3
sniirr <sniirr@gmail.com>
2022-09-14 13:24:51 +0300

    handle invite expiration

--- frontend/db/invite-db-manager.js
+++ frontend/db/invite-db-manager.js
@@ -2,19 +2,45 @@ const InviteModel = require("./models/InviteModel");
 const PasswordModel = require("./models/PasswordModel");
 const add = require("date-fns/add");
 
+const INVITE_TTL_SECONDS = 30;
+
+async function getAndValidateInvite(inviteId) {
+  let invite = await InviteModel.findById(inviteId);
+
+  if (
+    invite
+    && !invite.revoked
+    && invite.opened
+    && INVITE_TTL_SECONDS > 0) {
+
+    const expires = add(invite.opened, { seconds: INVITE_TTL_SECONDS })
+    console.log('Checking if invite expired', { invite, now: new Date(), opened: invite.opened, expires: expires })
+
+    if (expires < new Date()) {
+      console.log('Invite expired', { invite })
+      await InviteModel.findByIdAndUpdate(inviteId, { revoked: true })
+      invite = await InviteModel.findById(inviteId);
+    }
+
+    invite.expires = expires
+  }
+
+  return invite
+}
+
 async function openInvite(inviteId, country, state) {
   try {
     // check that the invite exist and not revoked or expired
-    let invite = await InviteModel.findById(inviteId);
+    const invite = await getAndValidateInvite(inviteId);
     if (!invite) {
       console.log("openInvite - invite not found", { inviteId });
       return null;
     }
+
     if (invite.revoked) {
       console.log("openInvite - invite revoked", { invite });
       return { invite };
     }
-    // TODO - handle invite expiration
 
     // get password
     const password = await PasswordModel.findOneAndUpdate(
@@ -26,12 +52,12 @@ async function openInvite(inviteId, country, state) {
       { _id: inviteId },
       {
         // revoked: true,
-        opened: Date.now(),
+        opened: invite.opened || Date.now(),
         location: `${state}, ${country}`,
       }
     );
 
-    return { invite: updatedInvite, password: password.password };
+    return { invite: updatedInvite, password: password.password, ttl: INVITE_TTL_SECONDS };
   } catch (e) {
     console.log(`Failed to create invite`, e);
   }
@@ -39,8 +65,9 @@ async function openInvite(inviteId, country, state) {
 
 async function isInviteRevoked(inviteId) {
   try {
-    const invite = await InviteModel.findById(inviteId).exec();
-    return invite.revoked;
+    const invite = await getAndValidateInvite(inviteId);
+
+    return !invite || invite.revoked;
   } catch (e) {
     console.log(`Failed to create invite`, e);
   }
@@ -57,14 +84,10 @@ async function isCorrectPwd(password) {
 
 async function getPassword(inviteId) {
   try {
-    const invite = await InviteModel.findById(inviteId);
+    const invite = await getAndValidateInvite(inviteId);
+
     console.log("Getting password for invite", { invite });
-    if (
-      invite &&
-      !invite.revoked &&
-      invite.opened &&
-      add(invite.opened, { hours: 12 }) > new Date()
-    ) {
+    if (invite && !invite.revoked && invite.opened) {
       const password = await PasswordModel.findOneAndUpdate(
         { status: "available" },
         { status: "pending" }
