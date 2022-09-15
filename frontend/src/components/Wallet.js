fbce81a2f4bfdaa02f8b0b0532e523173f56d4b5
sniirr <sniirr@gmail.com>
2022-09-15 18:11:38 +0300

    upgrade react-scripts, wagmi and rainbowkit

--- frontend/src/components/Wallet.js
+++ frontend/src/components/Wallet.js
@@ -2,7 +2,7 @@ import React from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const Wallet = () => {
-  return <ConnectButton accountStatus="address" showBalance={false} />;
+  return <ConnectButton accountStatus="address" chainStatus="none" showBalance={false} />;
 };
 
 export default Wallet;
--- frontend/src/containers/WagmiWrapper.js
+++ frontend/src/containers/WagmiWrapper.js
@@ -7,7 +7,7 @@ import {
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { infuraProvider } from "wagmi/providers/infura";
 import { publicProvider } from "wagmi/providers/public";
-import "@rainbow-me/rainbowkit/dist/index.css";
+import '@rainbow-me/rainbowkit/styles.css';
 
 const localChain = {
   id: 31337,
@@ -28,7 +28,7 @@ function WagmiWrapper({ children }) {
   const { chains, provider } = configureChains(
     [localChain, chain.goerli, chain.ropsten, chain.polygonMumbai],
     [
-      infuraProvider({ infuraId: "dbe63b3bdfc84f3abdf38cdc8e22f492" }),
+      infuraProvider({ apiKey: "dbe63b3bdfc84f3abdf38cdc8e22f492" }),
       publicProvider(),
     ]
   );
@@ -46,11 +46,12 @@ function WagmiWrapper({ children }) {
 
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains} theme={darkTheme()}>
+      <RainbowKitProvider chains={chains} theme={darkTheme()} modalSize="compact">
         {children}
       </RainbowKitProvider>
     </WagmiConfig>
   );
 }
+
 
 export default WagmiWrapper;
