df2a25563a27e5f4f9fc2dbf2d37be4059e5b484
sniirr <sniirr@gmail.com>
2022-10-03 21:57:40 +0300

    signInvite - change validation order

--- frontend/db/invite-db-manager.js
+++ frontend/db/invite-db-manager.js
@@ -63,16 +63,17 @@ async function openInvite(inviteId, country, state) {
 }
 
 async function signInvite(inviteId, address) {
+  if (!ethers.utils.isAddress(address)) {
+    throw new Error(
+      `signInvite failed - invalid Ethereum address - "${address}"`
+    );
+  }
+  
   // check that the invite exist and not revoked or expired
   const invite = await getInviteObjectById(inviteId);
   if (!invite || invite.revoked || invite.used) {
     throw new Error(
       `signInvite failed - invite not found, revoked or used - "${inviteId}"`
-    );
-  }
-  if (!ethers.utils.isAddress(address)) {
-    throw new Error(
-      `signInvite failed - invalid Ethereum address - "${address}"`
     );
   }
 
