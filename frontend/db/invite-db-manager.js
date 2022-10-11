eb807d502848a1ab87ade6313776207c1d8fbabf
sniirr <sniirr@gmail.com>
2022-10-11 20:13:21 +0300

    remove Signature model

--- frontend/db/invite-db-manager.js
+++ frontend/db/invite-db-manager.js
@@ -1,24 +1,26 @@
 const InviteModel = require("./models/InviteModel");
-const SignatureModel = require("./models/SignatureModel");
 const add = require("date-fns/add");
 const ethers = require("ethers");
-const isEmpty = require("lodash/isEmpty");
 
 const signer = new ethers.Wallet(process.env.SIGNER_PRIVATE_KEY);
 
 async function getSignatureByAddress(address) {
-  return await SignatureModel.findOne({ address });
+  // Convert provided `address` to correct checksum address format.
+  // This step is critical as signing an incorrectly formatted wallet address
+  // can result in invalid signatures when it comes to minting.
+  let addr = ethers.utils.getAddress(address);
+
+  // Create the message to be signed using the checksum formatted `addr` value.
+  let message = ethers.utils.arrayify(`0x${addr.slice(2).padStart(64, "0")}`);
+
+  // Sign the message using `signer`.
+  return await signer.signMessage(message);
 }
 
 async function getInviteObjectById(inviteId) {
   try {
     const invite = (await InviteModel.findById(inviteId)).toObject();
     if (!invite) return invite;
-
-    if (invite.address) {
-      const signature = await getSignatureByAddress(invite.address);
-      invite.signed = !isEmpty(signature);
-    }
 
     if (invite.opened && process.env.REACT_APP_INVITE_TTL_SECONDS > 0) {
       invite.expires = add(invite.opened, {
@@ -41,7 +43,7 @@ async function openInvite(inviteId, country, state) {
     // check that the invite exist and not revoked or expired
     const invite = await getInviteObjectById(inviteId);
     if (!invite || invite.used || invite.revoked || !invite.approved) {
-      console.log("openInvite - invalid invite", invite);
+      console.log("openInvite - Invalid invite", invite);
       return null;
     }
 
@@ -62,43 +64,23 @@ async function openInvite(inviteId, country, state) {
 async function signInvite(inviteId, address) {
   if (!ethers.utils.isAddress(address)) {
     throw new Error(
-      `signInvite failed - invalid Ethereum address - "${address}"`
+      `Invalid Ethereum address - "${address}"`
     );
   }
 
   // check that the invite exist and not revoked or expired
   const invite = await getInviteObjectById(inviteId);
   if (!invite || invite.revoked || invite.used || !invite.approved) {
-    throw new Error(`signInvite failed - invalid invite - "${inviteId}"`);
+    throw new Error(`Invalid invite`);
   }
 
-  let signature = await getSignatureByAddress(address);
-  let sig;
-  if (signature) {
-    sig = signature.sig;
-  } else {
-    // Convert provided `address` to correct checksum address format.
-    // This step is critical as signing an incorrectly formatted wallet address
-    // can result in invalid signatures when it comes to minting.
-    let addr = ethers.utils.getAddress(address);
-
-    // Create the message to be signed using the checksum formatted `addr` value.
-    let message = ethers.utils.arrayify(`0x${addr.slice(2).padStart(64, "0")}`);
-
-    // Sign the message using `signer`.
-    sig = await signer.signMessage(message);
-
-    // Save Signature to DB
-    await SignatureModel.create({ address, sig });
-
-    // Save address and n invite
-    await InviteModel.findOneAndUpdate({ _id: inviteId }, { address: address });
+  if (address !== invite.address) {
+    throw new Error(`Wrong Ethereum address`);
   }
 
-  return {
-    invite: await getInviteObjectById(inviteId),
-    signature: sig,
-  };
+  const signature = await getSignatureByAddress(address);
+
+  return { invite, signature };
 }
 
 async function confirmInviteUsed(inviteId) {
@@ -121,6 +103,9 @@ async function confirmInviteUsed(inviteId) {
 }
 
 async function createInviteRequest(address, identifier, country, state) {
+  if (!ethers.utils.isAddress(address)) {
+    throw new Error(`Invalid Ethereum address - "${address}"`);
+  }
   let invite = await InviteModel.findOne({ address });
   if (invite) {
     throw new Error("Address already invited");
