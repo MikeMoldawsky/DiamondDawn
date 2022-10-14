e20f15f8f48ab920763f64ae17ea20134f435584
Mike Moldawsky <mike.moldawsky@gmail.com>
2022-10-14 16:46:04 +0300

    fix

--- smart-contracts/contracts/DiamondDawn.sol
+++ smart-contracts/contracts/DiamondDawn.sol
@@ -139,7 +139,10 @@ contract DiamondDawn is
     }
 
     function rebirth(uint tokenId, bytes calldata signature) external isDawnAllowed(tokenId) {
-        require(_isValid(signature, tokenId), "Not allowed to rebirth");
+        require(
+            _isValid(signature, bytes32(abi.encodePacked(_msgSender(), uint96(tokenId)))),
+            "Not allowed to rebirth"
+        );
         _shipped[_msgSender()].remove(tokenId);
         ddMine.rebirth(tokenId);
         _safeMint(_msgSender(), tokenId);
@@ -210,31 +213,17 @@ contract DiamondDawn is
 
     /**********************     Private Functions     ************************/
 
-    function _isValid(bytes calldata signature, uint tokenId) private view returns (bool) {
-        return
-            _signer ==
-            keccak256(
-                abi.encodePacked(
-                    "\x19Ethereum Signed Message:\n32",
-                    bytes32(bytes32(abi.encodePacked(_msgSender(), uint96(tokenId))))
-                )
-            ).recover(signature);
-    }
-
-    function _isValid(bytes calldata signature) private view returns (bool) {
-        return
-            _signer ==
-            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", bytes32(uint256(uint160(_msgSender())))))
-                .recover(signature);
-    }
-
     function _enter(bytes calldata signature) private isActiveStage(Stage.INVITE) isNotFull {
-        require(_isValid(signature), "Not allowed to mint");
+        require(_isValid(signature, bytes32(uint256(uint160(_msgSender())))), "Not allowed to mint");
         // TODO: uncomment before production
         // require(!_minted[_msgSender()], "Already minted");
         _minted[_msgSender()] = true;
         uint256 tokenId = ++_numTokens;
         ddMine.enter(tokenId);
         _safeMint(_msgSender(), tokenId);
+    }
+
+    function _isValid(bytes calldata signature, byte32 message) private pure returns (bool) {
+        return _signer == keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", message)).recover(signature);
     }
 }
