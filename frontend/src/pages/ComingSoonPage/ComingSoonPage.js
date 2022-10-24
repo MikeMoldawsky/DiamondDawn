9e29ac617949d49d831ee2a342efc3ee7a2385ad
sniirr <sniirr@gmail.com>
2022-10-24 15:59:52 +0300

    Curtain animation (#252)
    * curtain animation - wip
    
    * curtain animation
    
    * format
    
    * horizontal curtain
    
    * fix bg position in horizontal curtain
    
    * on/off animations env var

--- frontend/src/pages/ComingSoonPage/ComingSoonPage.js
+++ frontend/src/pages/ComingSoonPage/ComingSoonPage.js
@@ -1,13 +1,17 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import "./ComingSoonPage.scss";
 import ReactPlayer from "react-player";
 import PasswordBox from "components/PasswordBox";
 import { updateUiState } from "store/uiReducer";
 import { useDispatch } from "react-redux";
 import { getCDNObjectUrl, isDemo } from "utils";
+import classNames from "classnames";
+import { useNavigate } from "react-router-dom";
 
 const ComingSoonPage = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [startTransition, setStartTransition] = useState(false);
 
   const renderBgPlayer = useCallback(
     () => (
@@ -26,13 +30,36 @@ const ComingSoonPage = () => {
     []
   );
 
+  const transition = () => {
+    if (!process.env.REACT_APP_ENABLE_TRANSITIONS) {
+      return navigate("/enter");
+    }
+
+    setStartTransition(true);
+
+    const EFFECT_TIME_MULTIPLIER = 1.7;
+
+    setTimeout(() => {
+      navigate("/enter");
+    }, 1450 * EFFECT_TIME_MULTIPLIER);
+  };
+
   const onCorrectPassword = () => {
     dispatch(updateUiState({ demoAuth: true }));
+    transition();
   };
 
   return (
-    <div className="page coming-soon">
+    <div
+      className={classNames("page coming-soon", {
+        horizontal: true,
+        "transition-out": startTransition,
+      })}
+    >
       {renderBgPlayer()}
+      <div className="curtain-left" />
+      <div className="curtain-right" />
+      <div className="curtain-behind" />
       <div className="center-aligned-column content">
         <div className="leading-text">COMING SOON</div>
         <div className="secondary-text">
@@ -40,7 +67,13 @@ const ComingSoonPage = () => {
           <br />
           Which diamond will you choose?
         </div>
-        {isDemo() && <PasswordBox onCorrect={onCorrectPassword} />}
+        {isDemo() ? (
+          <PasswordBox onCorrect={onCorrectPassword} />
+        ) : (
+          <div className="button" onClick={transition}>
+            EXPLORE
+          </div>
+        )}
       </div>
     </div>
   );
