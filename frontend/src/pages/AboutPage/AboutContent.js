d1f8d4dffc73edfa2e62c1a962545fa9787b5ba5
sniirr <sniirr@gmail.com>
2022-11-07 20:09:19 +0200

    move about content to another file

---
+++ frontend/src/pages/AboutPage/AboutContent.js
@@ -0,0 +1,207 @@
+import React from 'react'
+
+export const AboutDiamondDawnText = () => (
+  <>
+    <div className="text">
+      Diamond Dawn is a social experiment developed by a passionate team
+      that’s made up of software engineers, visual artists, and a
+      world-renowned 3D designer alongside one of the most respected
+      companies in the diamond industry.
+    </div>
+    <br />
+    <div className="text">
+      Your Diamond Dawn journey will consist of 4 steps.
+      <br />
+      <br />
+      At each step, you must choose whether to evolve your Diamond NFT
+      to its next form or keep it in its current state for eternity. You’ll have exactly <b>3 weeks, 3 days, and 3 hours</b> to
+      complete each step.
+    </div>
+    <br />
+    <br />
+    <div className="secondary-text">Which diamond will you choose?</div>
+    <br />
+    <div className="text">
+      In the last phase, you will be faced with <b>The Final Choice:</b>
+      <br />
+      Keep your diamond NFT digital or burn it in exchange for a
+      physical, limited-edition artwork that contains your diamond
+      (GIA-certified). <br /> <br />
+      Once your decision is finalized on the blockchain, your artwork
+      will be <b>shipped</b> to your specified address.
+    </div>
+    <br />
+    <div className="text">Physical vs. Digital</div>
+  </>
+)
+
+export const TheJourneyText = () => (
+  <div className="journey-desc text">
+    Diamond Dawn does NOT hold your NFT attributes on a centralized
+    server - the project is <b>entirely on-chain</b>.
+    <br />
+    Throughout this historical journey, you’ll learn about the hidden
+    world of the diamond industry, take part in the creation of the{" "}
+    <b>first digital diamonds</b> that live on the blockchain, and
+    discover the secrets behind their famous sparkle.
+    <br />
+    Diamond Dawn has 4 steps.
+  </div>
+)
+
+export const Step0Text = () => (
+  <div className="text">
+    If your application to join Diamond Dawn is accepted, you will
+    have a limited time window of{" "}
+    <b>3 days, 3 hours, and 3 minutes</b> to activate your key for
+    3.33 $ETH.
+    <br />
+    <br />
+    The key grants you <b>full access</b> to the 4 steps of DD's
+    journey, starting in the virtual mine, where your journey
+    begins.
+    <br />
+    <br />
+    The application phase will automatically end when one of the
+    following things happens:
+    <br />
+    <br />
+    <div className="text-center">
+      333 participants activated their keys
+      <br />
+      OR
+      <br />
+      The mine opens in...
+    </div>
+  </div>
+)
+
+export const Step1Text = () => (
+  <div className="text">The virtual mine opens in XX:XX</div>
+)
+
+export const Step2Text = () => null
+
+export const Step3Text = () => null
+
+export const Step4Text = () => (
+  <div className="text">
+    The final step is unique, and that's why you’ll have a longer
+    time window to decide about your actions. <br />
+    <br />
+    You'll have exactly <b>3 months, 3 weeks, and 3 days</b> to
+    decide.
+    <br />
+    <br />
+    In this final phase, you'll be faced with the ultimate choice:
+    <br />
+    Keep your diamond digital (NFT) or <b>burn</b> it in exchange
+    for your physical diamond artwork (GIA-certified).
+    <br />
+  </div>
+)
+
+export const OurTeamText = () => (
+  <div className="text our-team">
+    Diamond Dawn was created by a team of forward-thinking individuals
+    from various fields who share a common set of values.
+    <br />
+    <br />
+    We all believe that <b>decentralization</b> is key to the
+    Internet’s survival.
+    <br />
+    <br /> We look forward to the day when the robust technology
+    behind Web3 and NFTs will disrupt most industries as we know them
+    today, resulting in a shift in “power” from corporations to
+    individuals.
+    <br />
+    <br />
+    Diamond Dawn was specifically architectured to demonstrate how
+    decentralized technology can create a <b>REAL-LIFE</b> impact.
+  </div>
+)
+
+export const MikeText = () => (
+  <div className="text">
+    An entrepreneur with a background in software engineering,
+    Mike fell under the magic of NFT technology in 2016. An early
+    adopter by nature, Mike foresaw that the disruptive tech
+    behind web3 and NFTs will become a transforming force for most
+    industries, digital communities, and how we consume content on
+    the whole. He has since vowed to become an active member and
+    architect of the crypto revolution, making it his life’s
+    mission to push forward decentralized technology to a state
+    where it creates REAL LIFE impact that will, in turn, change
+    society at large.
+  </div>
+)
+
+export const DavidText = () => (
+  <div className="text">
+    A 3D artist and educator, David is widely known in the NFT
+    community. He has worked with artists like Beeple, Pak,
+    Deadmau5, Zedd, Katy Perry, Keith Urban, and Excision. He is
+    prolific in various artistic styles that he continues to
+    develop - from energetic concert visuals that are tightly
+    synced with the music, to creating overly ambitious, full CG
+    short films, and mesmerizing animations. David has also had
+    several major successes in web3, including being part of the
+    Pak's ASH 2 drop, creating two NFTs for Beeple x Madonn a (one
+    of which sold for $346,000) and a collab with famous artist
+    Jia Alli on a piece entitled "The Road Ahead", which sold at
+    Sotheby’s. David’s mission as an artist is not only to grow
+    the NFT community, but also to help up-and-coming 3D artists
+    gain traction in the field.
+  </div>
+)
+
+export const TonyText = () => (
+  <div className="text">
+    Born to diamond merchants, Aviva was exposed, from a young
+    age,  to both the harsh world of trade and negotiation and the
+    fascinating world of exquisite beauty contained within the
+    stones themselves. A world where, to this day, deals are
+    closed not with a contract, but with a handshake and the word
+    ‘luck’.  A highly-respected name in the world of diamonds, the
+    Moldawsky family has supplied stones to Tiffany’s, Sterling,
+    JC Penny, and more of  the largest firms in the world. As a
+    diamond designer, Aviva helps her clients choose the rough
+    stone, knowing which stone will yield the most desirable end
+    result, and oversees the entire design process that will best
+    fit her customer’s personality and style - from raw material
+    all the way to the box in which it is delivered.
+  </div>
+)
+
+export const AvivaText = () => (
+  <div className="text">
+    Born to diamond merchants, Aviva was exposed, from a young
+    age,  to both the harsh world of trade and negotiation and the
+    fascinating world of exquisite beauty contained within the
+    stones themselves. A world where, to this day, deals are
+    closed not with a contract, but with a handshake and the word
+    ‘luck’.  A highly-respected name in the world of diamonds, the
+    Moldawsky family has supplied stones to Tiffany’s, Sterling,
+    JC Penny, and more of  the largest firms in the world. As a
+    diamond designer, Aviva helps her clients choose the rough
+    stone, knowing which stone will yield the most desirable end
+    result, and oversees the entire design process that will best
+    fit her customer’s personality and style - from raw material
+    all the way to the box in which it is delivered.
+  </div>
+)
+
+export const AsafText = () => (
+  <div className="text">
+    Asaf has been a software developer for over 15 years, working
+    mostly in web2. Three years ago, he discovered the world of
+    blockchain technology and has worked exclusively on web3
+    projects since. Asaf is an active member in several
+    decentralized organizations (DAO). He is a frontend specialist
+    who believes decentralization is the key to the Internet’s
+    survival and its only logical next evolutionary step. He also
+    insists that music and art are what sets us apart and makes us
+    shine as a species. We agree wholeheartedly.
+  </div>
+)
+
