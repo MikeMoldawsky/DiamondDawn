47eea1d56f1ee37b2ec0ead259d2a6ae2458b9fa
sniirr <sniirr@gmail.com>
2022-11-10 02:15:30 +0200

    enter mine design

--- frontend/src/pages/ProcessPage/EnterMine/EnterMineView.js
+++ frontend/src/pages/ProcessPage/EnterMine/EnterMineView.js
@@ -20,33 +20,33 @@ const EnterMineView = ({
     <div className="action-view enter">
       <div className="layout-box">
         <div className="image-box" />
-        <div className="congrats-box">
-          <div className="center-aligned-column">
-            <div className="leading-text">CONGRATULATIONS!</div>
-            <div className="secondary-text">
-              <div>You have been selected to participate in Diamond Dawn.</div>
-              During your journey, you will decide when to proceed and when to
-              stop. At the end, you will be faced with the ultimate choice:
-              Which form of exquisite diamond art you value more, the virtual or
-              the tangible.
+        <div className="content-box">
+          <div className="congrats-box">
+            <div className="center-aligned-column">
+              <div className="leading-text">CONGRATULATIONS</div>
+              <div className="congrats-text">
+                You’ve been accepted to participate in Diamond Dawn’s historical journey.
+                <br/>
+                {/*In order to allow other participants to join, you’ll have exactly <b>3 days, 3 hours, and 3 minutes</b> to activate your key.*/}
+                {/*<br/>*/}
+                The key grants you <b>full access</b> to the 4 steps of DD's journey, starting in the virtual mine, where your journey begins.
+              </div>
             </div>
           </div>
-        </div>
-        <div className="mint-box">
-          <div className="center-aligned-row">
-            <div className="center-start-aligned-row text-row">
+          <div className="center-aligned-row mint-box">
+            <div className="center-start-aligned-row price-text">
               <FontAwesomeIcon icon={faEthereum} />
               <div className="price">
                 {BigNumber.isBigNumber(minePrice)
                   ? ethersUtils.formatUnits(minePrice)
-                  : "N/A"}{" "}
+                  : ""}{" "}
                 ACTIVATE YOUR KEY
               </div>
             </div>
             <div>
               <ActionButton
                 actionKey="EnterMine"
-                className="action-button"
+                className="action-button lg"
                 disabled={!canMint || !isFunction(enterMine)}
                 onClick={() => isFunction(enterMine) && enterMine()}
               >
@@ -54,28 +54,35 @@ const EnterMineView = ({
               </ActionButton>
             </div>
           </div>
-        </div>
-        <div className="timer-box">
-          <div className="text-comment">
-            The opportunity will close once the clock runs out
+          <div className="timer-box">
+            <div className="text-comment">
+              The opportunity will close once the clock runs out
+            </div>
+            <Countdown
+              parts={
+                expiresAt ? null : { days: 3, hours: 3, minutes: 3, seconds: 0 }
+              }
+              date={expiresAt}
+              onComplete={() => isFunction(onCountdownEnd) && onCountdownEnd()}
+              renderParts={{
+                days: true,
+                hours: true,
+                minutes: true,
+                seconds: true,
+              }}
+            />
           </div>
-          <Countdown
-            parts={
-              expiresAt ? null : { days: 3, hours: 3, minutes: 3, seconds: 0 }
-            }
-            date={expiresAt}
-            onComplete={() => isFunction(onCountdownEnd) && onCountdownEnd()}
-            renderParts={{
-              days: true,
-              hours: true,
-              minutes: true,
-              seconds: true,
-            }}
-          />
-          <div className="or">OR</div>
-        </div>
-        <div className="status-box">
-          {diamondCount} / {maxDiamonds} ALREADY MINTED
+          <div className="center-aligned-row invites-box">
+            <div className="keys-image">
+              <div className="key" />
+              <div className="key" />
+            </div>
+            <div className="text-sm">Now that you’re accepted you can share 2 unique links and invite other collectors to the private sale</div>
+            <div className="button gold">INVITE</div>
+          </div>
+          <div className="status-box">
+            {diamondCount} / {maxDiamonds} MINTED
+          </div>
         </div>
       </div>
     </div>
