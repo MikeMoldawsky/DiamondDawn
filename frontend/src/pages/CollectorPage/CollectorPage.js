81aba2ddd4501140860187f06631b59e64734f48
sniirr <sniirr@gmail.com>
2022-11-10 02:55:59 +0200

    PSR 6 prod (#290)
    * merge main into develop (#266)
    
    * fi Fix react-error-overlay dependency (#267)
    
    * Music, "Apply to" button, legal pages (#270)
    
    * header - "apply to" button
    
    * bg music
    
    * legal - t&c
    
    * privacy page
    
    * format
    
    * nit
    
    * nit (#273)
    
    * PSR 3 (#274)
    
    * page loader - images only
    
    * add videos, fonts and timeout to PageLoader
    
    * improve page loader
    
    * page size limit
    
    * fix page size limit
    
    * better PageSizeLimit for mobile
    
    * coming-soon mobile
    
    * remove local video
    
    * format
    
    * PSR 4 (#276)
    
    * fix "landscape" when keyboard is open in mobile
    
    * fix coming soon css
    
    * add new color
    
    * format
    
    * Fix diamond object to use uint64 for GIA number (#278)
    
    * about - wip (#279)
    
    * Nit: Diamond Down -> Diamond Dawn (#281)
    
    * PSR5 prod (#280) (#282)
    
    * Which diamond text goes up - about page (#284)
    
    * PSR 5 (#287)
    
    * about - wip
    
    * background positioning
    
    * move about content to another file
    
    * about page - wip
    
    * about - remaining content
    
    * about - responsiveness
    
    * format
    
    * PSR 6 (#289)
    
    * cleanup
    
    * move assets to cdn
    
    * minor stuff
    
    * coming soon text color
    
    * collector's page - css + submitted modal
    
    * enter mine design
    
    * format
    
    * checkbox style
    
    Co-authored-by: MikeMoldawsky <mike.moldawsky@gmail.com>

--- frontend/src/pages/CollectorPage/CollectorPage.js
+++ frontend/src/pages/CollectorPage/CollectorPage.js
@@ -11,10 +11,11 @@ import Box from "components/Box";
 import Suspense from "components/Suspense";
 import Invite from "components/Invite";
 import NFTs from "components/NFTs";
-import { getCDNImageUrl, isDemo } from "utils";
+import { getCDNImageUrl, isDemo, shortenEthAddress } from "utils";
 import useMusic from "hooks/useMusic";
 import PageLoader from "components/PageLoader";
 import PageSizeLimit from "components/PageSizeLimit";
+import EditIcon from "@mui/icons-material/Edit";
 
 const CollectorPage = () => {
   const tokens = useSelector(tokensSelector);
@@ -50,6 +51,9 @@ const CollectorPage = () => {
         <div className={classNames("page collector-page")}>
           <div className="inner-page">
             <h1>The Collector's Room</h1>
+            <div className="center-center-aligned-row account">
+              Hello - {shortenEthAddress(account?.address)} <EditIcon />
+            </div>
             <Box className={"main-box"}>
               <Suspense withLoader actions={suspenseActions}>
                 {renderContent()}
