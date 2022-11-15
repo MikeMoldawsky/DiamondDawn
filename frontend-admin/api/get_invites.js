e821a29c5b049442aada8043d0da3650eeaa197c
sniirr <sniirr@gmail.com>
2022-11-15 09:28:25 +0200

    cleanup

--- frontend-admin/api/get_invites.js
+++
@@ -1,6 +0,0 @@
-const { getInvites } = require("../db/invite-db-manager");
-
-module.exports = async function (req, res) {
-  const invites = await getInvites(req.body?.approved);
-  res.send(invites);
-};
