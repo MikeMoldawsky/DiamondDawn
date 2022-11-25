67abb8e7a79794967f537623ad25e8d9aa670b6b
sniirr <sniirr@gmail.com>
2022-11-26 00:48:17 +0200

    homepage mousemove parallax

--- frontend/src/components/HomeBackground/HomeBackground.js
+++ frontend/src/components/HomeBackground/HomeBackground.js
@@ -2,38 +2,45 @@ import React from "react";
 import "./HomeBackground.scss";
 import { useSelector } from "react-redux";
 import { uiSelector } from "store/uiReducer";
+import useWindowDimensions from "hooks/useWindowDimensions";
 
-const HomeBackground = () => {
+const HomeBackground = ({ mousePos }) => {
   const { scroll } = useSelector(uiSelector);
+  const { width } = useWindowDimensions();
 
-  const parallaxRef = 0.5;
+  const parallaxRef = 0.05;
+
+  const scrollWithMouse = scroll * 12 + mousePos[1] / 2
 
   return (
     <div className="bg home-background">
-      <div className="bg leaves-left" />
-      <div className="bg leaves-right" />
-      <div
-        className="bg sky"
-        style={{
-          transform: `translate3D(0px, -${(scroll * parallaxRef) / 10}px, 0px)`,
-        }}
+      <div className="bg leaves-left"
+           style={{
+             transform: `translate3D(${(width - mousePos[0] * 8) / 90}px, -${scrollWithMouse * parallaxRef * 1.5}px, 0px)`,
+           }}
       />
+      <div className="bg leaves-right"
+           style={{
+             transform: `translate3D(${((width - mousePos[0] * 8) / 90) + width / 40}px, -${scrollWithMouse * parallaxRef * 1.5}px, 0px)`,
+           }}
+      />
+      <div className="bg sky" />
       <div
         className="bg mountains-back"
         style={{
-          transform: `translate3D(0px, -${(scroll * parallaxRef) / 5}px, 0px)`,
+          transform: `translate3D(${(width - mousePos[0] * 1) / 90}px, -${(scrollWithMouse * parallaxRef) / 5}px, 0px)`,
         }}
       />
       <div
         className="bg mountains-front"
         style={{
-          transform: `translate3D(0px, -${scroll * parallaxRef}px, 0px)`,
+          transform: `translate3D(${(width - mousePos[0] * 4) / 90}px, -${scrollWithMouse * parallaxRef}px, 0px)`,
         }}
       />
       <div
         className="bg black-box"
         style={{
-          transform: `translate3D(0px, -${scroll * parallaxRef}px, 0px)`,
+          transform: `translate3D(0px, -${scrollWithMouse * parallaxRef}px, 0px)`,
         }}
       />
       <div className="bg stars" />
--- frontend/src/pages/Homepage/Homepage.js
+++ frontend/src/pages/Homepage/Homepage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from "react";
+import React, {useEffect, useMemo, useState} from "react";
 import "./Homepage.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { setSelectedTokenId, uiSelector } from "store/uiReducer";
@@ -29,8 +29,9 @@ const Homepage = () => {
   const countdownProps = useMineOpenCountdown();
   const dispatch = useDispatch();
   const { scroll } = useSelector(uiSelector);
-  const { height } = useWindowDimensions();
+  const { width, height } = useWindowDimensions();
   useShowLogoOnScroll(3.5);
+  const [mousePos, setMousePos] = useState([width / 2, height / 2])
 
   useMusic("homepage.mp3");
 
@@ -61,8 +62,10 @@ const Homepage = () => {
       >
         <ScrollingPage className="homepage">
           <div className="page homepage">
-            <div className="top-content center-aligned-column">
-              <HomeBackground />
+            <div className="top-content center-aligned-column"
+                 onMouseMove={e => setMousePos([e.pageX, e.pageY])}
+            >
+              <HomeBackground mousePos={mousePos} />
               <div className="common-view" style={topViewStyles}>
                 <div className="logo">
                   <AnimatedLogo withText />
