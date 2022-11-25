0dcde62d8b5807cb92772ab1434019c6bb103d59
Mike Moldawsky <mike.moldawsky@gmail.com>
2022-11-25 22:40:29 +0300

    fix

--- frontend/src/pages/TechPage/TechPageContent.js
+++ frontend/src/pages/TechPage/TechPageContent.js
@@ -170,8 +170,8 @@ export const Decentralized99 = () => (
     <p>
       Once the project is complete, we’ll trigger our lockDiamondDawn function
       to “set free” the Diamond Dawn project. This will remove our admin
-      permissions, thus completing DD’s transition from 99% decentralization -
-      to a 100% fully decentralized project.{" "}
+      permissions, thus completing DD’s transition from 99% decentralization to
+      a 100% fully decentralized project.{" "}
     </p>
     <p>
       The effect will be <b>100% immutability</b> for the rest of the time, and
