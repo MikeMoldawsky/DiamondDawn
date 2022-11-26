7698398603981112b425d3b0704d0d8850fe5e3f
MikeMoldawsky <mike.moldawsky@gmail.com>
2022-11-26 20:57:43 +0200

    Measure DB (#336)

--- frontend/api/get_contract.js
+++ frontend/api/get_contract.js
@@ -1,6 +1,9 @@
 const { getDiamondDawnContract } = require("../db/contract-db-manager");
 
 module.exports = async function (req, res) {
+  const start = Date.now();
   const contract = await getDiamondDawnContract();
+  const end = Date.now();
+  console.log(`Execution time: ${end - start} ms`);
   res.send(JSON.stringify(contract));
 };
