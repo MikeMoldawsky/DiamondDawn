3c62cd89eabd339a6bf04160f3830e641dd21922
sniirr <sniirr@gmail.com>
2022-11-26 23:22:21 +0200

    PSR 18 (#337)
    * favicon
    
    * NetworkGuard
    
    * fix image onload not triggering react
    
    * html description
    
    * format

---
+++ frontend/src/components/PageCover.js
@@ -0,0 +1,30 @@
+import React from "react";
+import useNoScrollView from "hooks/useNoScrollView";
+import classNames from "classnames";
+import Loading from "components/Loading";
+
+const PageCover = ({
+  fade,
+  showText,
+  title = "DIAMOND DAWN",
+  text = "loading...",
+}) => {
+  useNoScrollView();
+
+  return (
+    <div
+      className={classNames("center-aligned-column page-cover", {
+        hide: fade,
+      })}
+    >
+      <Loading />
+      <div className="secondary-text">
+        {showText && title}
+        <br />
+        {showText && text}
+      </div>
+    </div>
+  );
+};
+
+export default PageCover;
---
+++ frontend/src/containers/NetworkGuard.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { useNetwork } from "wagmi";
+import get from "lodash/get";
+import map from "lodash/map";
+import includes from "lodash/includes";
+import { chainNameById } from "utils";
+import PageCover from "components/PageCover";
+
+const SHOW_TEXT_TIME = 100;
+
+const NetworkGuard = ({ children }) => {
+  const { chain, chains } = useNetwork();
+  const [showText, setShowText] = useState(false);
+
+  // TODO - remove this after setting up app-shell caching
+  setTimeout(() => {
+    setShowText(true);
+  }, SHOW_TEXT_TIME);
+
+  if (chain?.id && !includes(map(chains, "id"), chain.id))
+    return (
+      <PageCover
+        showText={showText}
+        title={<span className="text-red">WRONG NETWORK</span>}
+        text={
+          <span className="">
+            please switch to {chainNameById(get(chains, 0, "id"))}
+          </span>
+        }
+      />
+    );
+
+  return children;
+};
+
+export default NetworkGuard;
