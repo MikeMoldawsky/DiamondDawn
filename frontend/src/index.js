c9f4b4240bef1d445a949b697ebeb6ee88884139
MikeMoldawsky <mike.moldawsky@gmail.com>
2022-11-27 16:52:45 +0200

    report analytics vercel(#346)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
 import App from "containers/App";
 import { Provider } from "react-redux";
 import { makeStore } from "store/makeStore";
 import { inject } from "@vercel/analytics";
+import reportWebVitals from "./reportWebVitals";
+import { sendToVercelAnalytics } from "./vitals";
 
 let store = makeStore();
 
@@ -18,6 +19,6 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(sendToVercelAnalytics);
 // Make sure to call this only once in your app
 inject();
