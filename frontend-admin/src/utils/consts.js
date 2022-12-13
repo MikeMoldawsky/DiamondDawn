b893fdd83438c4d4adee15edb944699d305b7c46
MikeMoldawsky <mike.moldawsky@gmail.com>
2022-12-13 13:20:55 +0200

    Mm/addd doiamonds new (#407)
    * add diamonds
    
    * add
    
    * erupt fix
    
    Co-authored-by: sniirr <sniirr@gmail.com>

---
+++ frontend-admin/src/utils/consts.js
@@ -0,0 +1,55 @@
+const SHAPE = {
+  PEAR: 1,
+  ROUND: 2,
+  OVAL: 3,
+  CUSHION: 4,
+};
+
+const GRADE = {
+  EMPTY: 0,
+  GOOD: 1,
+  VG: 2,
+  EX: 3,
+};
+
+const CLARITY = {
+  VS2: 1,
+  VS1: 2,
+  VVS2: 3,
+  VVS1: 4,
+  IF: 5,
+  FL: 6,
+};
+
+const FLUORESCENCE = {
+  FAINT: 1,
+  NONE: 2,
+};
+
+const COLOR = {
+  EMPTY: 0,
+  K: 1,
+  L: 2,
+  M: 3,
+  N: 4,
+  O: 5,
+  P: 6,
+  Q: 7,
+  R: 8,
+  S: 9,
+  T: 10,
+  U: 11,
+  V: 12,
+  W: 13,
+  X: 14,
+  Y: 15,
+  Z: 16,
+};
+
+module.exports = {
+  SHAPE,
+  GRADE,
+  CLARITY,
+  FLUORESCENCE,
+  COLOR,
+};
