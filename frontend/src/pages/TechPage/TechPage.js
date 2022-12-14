4eb1c9ac9df7162f8df41c7f4e144416e5cc5c87
sniirr <sniirr@gmail.com>
2022-12-15 01:17:38 +0200

    mobile - tech page

--- frontend/src/pages/TechPage/TechPage.js
+++ frontend/src/pages/TechPage/TechPage.js
@@ -15,61 +15,58 @@ import InfoPage from "components/InfoPage";
 import AnimatedText from "components/AnimatedText";
 import { getEarthAndMoonVideo } from "assets/videos";
 import useWindowDimensions from "hooks/useWindowDimensions";
-import PageSizeLimit from "components/PageSizeLimit";
 
 const TechPage = () => {
   const { width, height } = useWindowDimensions();
 
   return (
-    <PageSizeLimit>
-      <InfoPage
-        className="tech-page"
-        teaser={{ src: getEarthAndMoonVideo(width, height), overlap: "-35%" }}
-      >
-        <div className="left-aligned-column general">
-          <div className="leading-text">Timeless Technology</div>
-          <TimelessTechnology />
-        </div>
-        <div className="left-top-aligned-column content-section">
-          <div className="subtitle-text">Diamonds Are Forever</div>
-          <AnimatedText className="text">
-            <DiamondsAreForever />
-          </AnimatedText>
-          <div className="tagline-text">The Weakest Link</div>
-          <AnimatedText className="text">
-            <TheWeakestLink />
-          </AnimatedText>
-          <div className="tagline-text">Decentralization</div>
-          <AnimatedText className="text">
-            <Decentralization />
-          </AnimatedText>
-          <div className="tagline-text">Timeless NFT</div>
-          <AnimatedText className="text">
-            <TimelessNFT />
-          </AnimatedText>
-          <div className="tagline-text">Ethereum's Limit</div>
-          <AnimatedText className="text">
-            <EthereumLimit />
-          </AnimatedText>
-          <div className="tagline-text">Decentralized Files</div>
-          <AnimatedText className="text">
-            <DecentralizedFiles />
-          </AnimatedText>
-        </div>
-        <div className="left-top-aligned-column content-section">
-          <div className="subtitle-text">99% Decentralized</div>
-          <AnimatedText className="text">
-            <Decentralized99 />
-          </AnimatedText>
-        </div>
-        <div className="left-top-aligned-column content-section">
-          <div className="subtitle-text">On-Chain Scarcity</div>
-          <AnimatedText className="text">
-            <OnChainScarcity />
-          </AnimatedText>
-        </div>
-      </InfoPage>
-    </PageSizeLimit>
+    <InfoPage
+      className="tech-page"
+      teaser={{ src: getEarthAndMoonVideo(width, height), overlap: "-35%" }}
+    >
+      <div className="left-aligned-column general">
+        <div className="leading-text">Timeless Technology</div>
+        <TimelessTechnology />
+      </div>
+      <div className="left-top-aligned-column content-section">
+        <div className="subtitle-text">Diamonds Are Forever</div>
+        <AnimatedText className="text">
+          <DiamondsAreForever />
+        </AnimatedText>
+        <div className="tagline-text">The Weakest Link</div>
+        <AnimatedText className="text">
+          <TheWeakestLink />
+        </AnimatedText>
+        <div className="tagline-text">Decentralization</div>
+        <AnimatedText className="text">
+          <Decentralization />
+        </AnimatedText>
+        <div className="tagline-text">Timeless NFT</div>
+        <AnimatedText className="text">
+          <TimelessNFT />
+        </AnimatedText>
+        <div className="tagline-text">Ethereum's Limit</div>
+        <AnimatedText className="text">
+          <EthereumLimit />
+        </AnimatedText>
+        <div className="tagline-text">Decentralized Files</div>
+        <AnimatedText className="text">
+          <DecentralizedFiles />
+        </AnimatedText>
+      </div>
+      <div className="left-top-aligned-column content-section">
+        <div className="subtitle-text">99% Decentralized</div>
+        <AnimatedText className="text">
+          <Decentralized99 />
+        </AnimatedText>
+      </div>
+      <div className="left-top-aligned-column content-section">
+        <div className="subtitle-text">On-Chain Scarcity</div>
+        <AnimatedText className="text">
+          <OnChainScarcity />
+        </AnimatedText>
+      </div>
+    </InfoPage>
   );
 };
 
