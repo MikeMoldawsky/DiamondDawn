030188122d485bddc8fe7beaeada3813060bbc93
sniirr <sniirr@gmail.com>
2022-12-15 01:27:24 +0200

    tech page cleanup

--- frontend/src/pages/TechPage/TechPage.js
+++ frontend/src/pages/TechPage/TechPage.js
@@ -29,42 +29,48 @@ const TechPage = () => {
         <TimelessTechnology />
       </div>
       <div className="left-top-aligned-column content-section">
-        <div className="subtitle-text">Diamonds Are Forever</div>
-        <AnimatedText className="text">
-          <DiamondsAreForever />
-        </AnimatedText>
-        <div className="tagline-text">The Weakest Link</div>
-        <AnimatedText className="text">
-          <TheWeakestLink />
-        </AnimatedText>
-        <div className="tagline-text">Decentralization</div>
-        <AnimatedText className="text">
-          <Decentralization />
-        </AnimatedText>
-        <div className="tagline-text">Timeless NFT</div>
-        <AnimatedText className="text">
-          <TimelessNFT />
-        </AnimatedText>
-        <div className="tagline-text">Ethereum's Limit</div>
-        <AnimatedText className="text">
-          <EthereumLimit />
-        </AnimatedText>
-        <div className="tagline-text">Decentralized Files</div>
-        <AnimatedText className="text">
-          <DecentralizedFiles />
-        </AnimatedText>
+        <div className="left-top-aligned-column">
+          <div className="subtitle-text">Diamonds Are Forever</div>
+          <AnimatedText className="text">
+            <DiamondsAreForever />
+          </AnimatedText>
+          <div className="tagline-text">The Weakest Link</div>
+          <AnimatedText className="text">
+            <TheWeakestLink />
+          </AnimatedText>
+          <div className="tagline-text">Decentralization</div>
+          <AnimatedText className="text">
+            <Decentralization />
+          </AnimatedText>
+          <div className="tagline-text">Timeless NFT</div>
+          <AnimatedText className="text">
+            <TimelessNFT />
+          </AnimatedText>
+          <div className="tagline-text">Ethereum's Limit</div>
+          <AnimatedText className="text">
+            <EthereumLimit />
+          </AnimatedText>
+          <div className="tagline-text">Decentralized Files</div>
+          <AnimatedText className="text">
+            <DecentralizedFiles />
+          </AnimatedText>
+        </div>
       </div>
       <div className="left-top-aligned-column content-section">
-        <div className="subtitle-text">99% Decentralized</div>
-        <AnimatedText className="text">
-          <Decentralized99 />
-        </AnimatedText>
+        <div className="left-top-aligned-column">
+          <div className="subtitle-text">99% Decentralized</div>
+          <AnimatedText className="text">
+            <Decentralized99 />
+          </AnimatedText>
+        </div>
       </div>
       <div className="left-top-aligned-column content-section">
-        <div className="subtitle-text">On-Chain Scarcity</div>
-        <AnimatedText className="text">
-          <OnChainScarcity />
-        </AnimatedText>
+        <div className="left-top-aligned-column">
+          <div className="subtitle-text">On-Chain Scarcity</div>
+          <AnimatedText className="text">
+            <OnChainScarcity />
+          </AnimatedText>
+        </div>
       </div>
     </InfoPage>
   );
