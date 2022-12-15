9f7d54be937128c8a9436539e567421b9a8fcf25
sniirr <sniirr@gmail.com>
2022-12-15 16:29:50 +0200

    final mobile stuff for prod (#418)
    * final mobile stuff for prod
    
    * delete PageSizeLimit

--- frontend/src/components/InfoPage/InfoPage.js
+++ frontend/src/components/InfoPage/InfoPage.js
@@ -6,33 +6,30 @@ import classNames from "classnames";
 import { getCDNImageUrl } from "utils";
 import Page from "containers/Page";
 import useMusic from "hooks/useMusic";
-import PageSizeLimit from "components/PageSizeLimit";
 
 const InfoPage = ({ className, teaser, children }) => {
   useMusic("homepage.mp3");
 
   return (
-    <PageSizeLimit>
-      <Page
-        pageName="info"
-        images={[
-          getCDNImageUrl("/about/mine-walls.png"),
-          getCDNImageUrl("/cave.jpg"),
-        ]}
-      >
-        <div className={classNames("page info-page", className)}>
-          <div className="inner-page">
-            <div className="bg mine-background">
-              <div className="bg walls" />
-              <div className="bg cave" />
-            </div>
-            {children}
-            {teaser && <VideoBackground {...teaser} />}
-            <Footer />
+    <Page
+      pageName="info"
+      images={[
+        getCDNImageUrl("/about/mine-walls.png"),
+        getCDNImageUrl("/cave.jpg"),
+      ]}
+    >
+      <div className={classNames("page info-page", className)}>
+        <div className="inner-page">
+          <div className="bg mine-background">
+            <div className="bg walls" />
+            <div className="bg cave" />
           </div>
+          {children}
+          {teaser && <VideoBackground {...teaser} />}
+          <Footer />
         </div>
-      </Page>
-    </PageSizeLimit>
+      </div>
+    </Page>
   );
 };
 
--- frontend/src/pages/FAQsPage/FAQsPage.js
+++ frontend/src/pages/FAQsPage/FAQsPage.js
@@ -2,18 +2,15 @@ import React from "react";
 import FAQs from "components/FAQs";
 import Footer from "components/Footer";
 import "./FAQsPage.scss";
-import PageSizeLimit from "components/PageSizeLimit";
 
 const FAQsPage = () => (
-  <PageSizeLimit>
-    <div className="page faqs-page">
-      <div className="inner-page">
-        <div className="leading-text">FAQs</div>
-        <FAQs />
-      </div>
-      <Footer />
+  <div className="page faqs-page">
+    <div className="inner-page">
+      <div className="leading-text">FAQs</div>
+      <FAQs />
     </div>
-  </PageSizeLimit>
+    <Footer />
+  </div>
 );
 
 export default FAQsPage;
