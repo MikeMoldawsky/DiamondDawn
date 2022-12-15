b267eb59f4088f7c1cdec132f1fb1a9eb7d0e012
sniirr <sniirr@gmail.com>
2022-12-15 02:45:44 +0200

    mobile - faqs page

--- frontend/src/pages/FAQsPage/FAQsPage.js
+++ frontend/src/pages/FAQsPage/FAQsPage.js
@@ -1,19 +1,16 @@
 import React from "react";
-import PageSizeLimit from "components/PageSizeLimit";
 import FAQs from "components/FAQs";
 import Footer from "components/Footer";
 import "./FAQsPage.scss";
 
 const FAQsPage = () => (
-  <PageSizeLimit>
-    <div className="page faqs-page">
-      <div className="inner-page">
-        <div className="leading-text">FAQs</div>
-        <FAQs />
-      </div>
-      <Footer />
+  <div className="page faqs-page">
+    <div className="inner-page">
+      <div className="leading-text">FAQs</div>
+      <FAQs />
     </div>
-  </PageSizeLimit>
+    <Footer />
+  </div>
 );
 
 export default FAQsPage;
