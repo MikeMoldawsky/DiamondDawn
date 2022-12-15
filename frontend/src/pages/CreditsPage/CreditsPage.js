eb9a763534c9ad48868c0562f67e785263984425
sniirr <sniirr@gmail.com>
2022-12-15 14:54:06 +0200

    credits page improvement

--- frontend/src/pages/CreditsPage/CreditsPage.js
+++ frontend/src/pages/CreditsPage/CreditsPage.js
@@ -4,43 +4,44 @@ import Footer from "components/Footer";
 import map from "lodash/map";
 import Link from "components/Links";
 import PageSizeLimit from "components/PageSizeLimit";
+import {getCDNImageUrl} from "utils";
+import classNames from "classnames";
 
-const CREDITS = {
-  "Project Creator": [
-    {
-      name: "Mike Moldawsky",
-      link: "https://www.linkedin.com/in/mike-moldawsky-608a2098/",
-    },
-  ],
-  Artist: [{ name: "David Ariew", link: "https://twitter.com/DavidAriew" }],
-  "Frontend Wizard": [
-    {
-      name: "Asaf Snir",
-      link: "https://www.linkedin.com/in/asaf-snir-87b581b6/",
-    },
-  ],
-  Designer: [{ name: "Aviva Moldawsky", link: "" }],
-  Advisor: [
-    { name: "Tony Herrera", link: "https://twitter.com/tonyherrera" },
-    { name: "Daniel Moldawsky", link: "https://twitter.com/DanielMoldawsky" },
-  ],
-  "Sound Wizard": [
-    { name: "Abel Okugawa", link: "https://twitter.com/Abel_Okugawa" },
-  ],
-  Marketing: [{ name: "Gal Shriki", link: "https://twitter.com/Shrikmeister" }],
-  "Art Piece Videos": [{ name: "Zen", link: "https://www.zenvideopro.com" }],
-  "Smart Contracts": [
-    {
-      name: "Gal Kleinman",
-      link: "https://www.linkedin.com/in/gal-kleinman-16898384/",
-    },
-  ],
-  "Smart Contracts Audit": [
-    { name: "Alan Boost", link: "https://twitter.com/ltsCuzzo" },
-  ],
-  "Web Designer": [{ name: "Yanay Halevy", link: "" }],
-  "Project Manager": [{ name: "Mor Lupa", link: "" }],
-};
+const CREDITS = [
+  {
+    id: "mike",
+    title: "Project Creator",
+    name: "Mike Moldawsky",
+    link: "https://www.linkedin.com/in/mike-moldawsky-608a2098/",
+  },
+  {
+    id: "david",
+    title: "Artist",
+    name: "David Ariew",
+    link: "https://twitter.com/DavidAriew",
+  },
+  {
+    id: "asaf",
+    title: "Frontend Master",
+    name: "Asaf Snir",
+    link: "https://www.linkedin.com/in/asaf-snir-87b581b6/",
+  },
+  { id: "aviva", title: "Designer", name: "Aviva Moldawsky", link: "" },
+  { id: "tony", title: "Advisor", name: "Tony Herrera", link: "https://twitter.com/tonyherrera" },
+  { id: "daniel", title: "Advisor", name: "Daniel Moldawsky", link: "https://twitter.com/DanielMoldawsky" },
+  { id: "abel", title: "Sound Wizard", name: "Abel Okugawa", link: "https://twitter.com/Abel_Okugawa" },
+  { id: "gals", title: "Marketing", name: "Gal Shriki", link: "https://twitter.com/Shrikmeister" },
+  { id: "zen", title: "Art Piece Videos", name: "Zen", link: "https://www.zenvideopro.com" },
+  {
+    id: "galk",
+    title: "Smart Contracts",
+    name: "Gal Kleinman",
+    link: "https://www.linkedin.com/in/gal-kleinman-16898384/",
+  },
+  { id: "alan", title: "Smart Contracts Audit", name: "Alan Boost", link: "https://twitter.com/ltsCuzzo" },
+  { id: "yanay", title: "Web Designer", name: "Yanay Halevy", link: "" },
+  { id: "mor", title: "Project Manager", name: "Mor Lupa", link: "" },
+]
 
 const CreditsPage = () => (
   <PageSizeLimit>
@@ -48,18 +49,19 @@ const CreditsPage = () => (
       <div className="inner-page">
         <h1>CREDITS</h1>
         <div className="credits">
-          {map(CREDITS, (creditsTo, category) => (
+          {map(CREDITS, ({ id, title, name, link }) => (
             <div
-              key={`credits-category-${category}`}
-              className="top-spaced-row credits-category"
+              key={`credits-category-${name}`}
+              className="start-start-aligned-row credits-category"
             >
-              <div className="category-name">{category}</div>
-              <div className="right-spaced-column">
-                {map(creditsTo, ({ name, link }) => (
+              <div className={classNames("profile-image", id)} />
+              <div className="left-top-aligned-column">
+                <div className="category-name">{title}</div>
+                <div className="right-spaced-column">
                   <div key={`credit-to-${name}`} className="credit-to">
                     <Link href={link}>{name}</Link>
                   </div>
-                ))}
+                </div>
               </div>
             </div>
           ))}
