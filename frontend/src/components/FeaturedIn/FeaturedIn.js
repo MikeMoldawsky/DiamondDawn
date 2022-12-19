f74791afd4f6a3d78962df4e519f7ad52a71605f
sniirr <sniirr@gmail.com>
2022-12-19 16:32:57 +0200

    format

--- frontend/src/components/FeaturedIn/FeaturedIn.js
+++ frontend/src/components/FeaturedIn/FeaturedIn.js
@@ -1,17 +1,29 @@
-import React from 'react'
-import "./FeaturedIn.scss"
-import map from "lodash/map"
+import React from "react";
+import "./FeaturedIn.scss";
+import map from "lodash/map";
 import Link from "components/Links";
 import SVG from "components/SVG";
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import logos from "assets/images/featured"
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import logos from "assets/images/featured";
 
 const FEATURED_IN = [
-  { name: "fortune", link: "https://fortune.com/crypto/2022/12/09/this-week-in-the-metaverse-alameda-research-ceo-reportedly-spotted-china-nfts-atlanta-braves-metaverse/" },
-  { name: "cointelegraph", link: "https://cointelegraph.com/news/nfts-could-help-solve-diamond-certification-fraud" },
-  { name: "coinmarketcap", link: "https://coinmarketcap.com/community/articles/639d8e9ba9652350656a2720" },
-  { name: "investing", link: "https://m.investing.com/news/cryptocurrency-news/nfts-could-help-solve-diamond-certification-fraud-2961569" },
-]
+  {
+    name: "fortune",
+    link: "https://fortune.com/crypto/2022/12/09/this-week-in-the-metaverse-alameda-research-ceo-reportedly-spotted-china-nfts-atlanta-braves-metaverse/",
+  },
+  {
+    name: "cointelegraph",
+    link: "https://cointelegraph.com/news/nfts-could-help-solve-diamond-certification-fraud",
+  },
+  {
+    name: "coinmarketcap",
+    link: "https://coinmarketcap.com/community/articles/639d8e9ba9652350656a2720",
+  },
+  {
+    name: "investing",
+    link: "https://m.investing.com/news/cryptocurrency-news/nfts-could-help-solve-diamond-certification-fraud-2961569",
+  },
+];
 
 const FeaturedIn = () => {
   return (
@@ -23,14 +35,18 @@ const FeaturedIn = () => {
       </div>
       <div className="center-aligned-row articles">
         {map(FEATURED_IN, ({ name, link }) => (
-          <Link className="center-aligned-row article" key={`featured-article-${name}`} href={link}>
+          <Link
+            className="center-aligned-row article"
+            key={`featured-article-${name}`}
+            href={link}
+          >
             <SVG src={logos[name]} className={name} wrapper="div" />
             <ArrowForwardIosIcon />
           </Link>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FeaturedIn
+export default FeaturedIn;
