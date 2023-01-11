1d6da83af417837c8f320b9d9c0943a8d9dddcba
sniirr <sniirr@gmail.com>
2023-01-11 15:06:27 +0200

    fix mobile transition - gallery to nft

--- frontend/src/components/NFTs/NFTGallery.js
+++ frontend/src/components/NFTs/NFTGallery.js
@@ -3,12 +3,13 @@ import map from "lodash/map";
 import { useSelector } from "react-redux";
 import { tokensSelector } from "store/tokensReducer";
 import DiamondThumbnail from "components/DiamondThumbnail";
+import classNames from "classnames";
 
 const NFTGallery = ({ goToProcess, selectToken }) => {
   const tokens = useSelector(tokensSelector);
 
   return (
-    <div className="center-aligned-row gallery">
+    <div className={classNames("center-aligned-row gallery")}>
       {map(tokens, (token) => (
         <div className="gallery-item">
           <DiamondThumbnail
